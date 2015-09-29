e09e2c98e49db5562c31785a922be0f4701bd8c7
Erika Lewis <erikawithak@Erikas-MacBook-Pro.local>
2015-09-29 09:24:17 -0400

    added acf-repeater, fixed archive-webinars conflicts, single-ercs

--- wp-content/themes/eschoolnews/archive-webinars.php
+++ wp-content/themes/eschoolnews/archive-webinars.php
@@ -25,29 +25,152 @@ get_header(); ?>
 <!-- Row for main content area -->
 	<div class="small-12 large-8 columns" role="main">
 
+<?php 	$args = array('post_type' => array( 'Webinars' ));
+		$query = new WP_Query( $args );
+		$count = 0;
+			if ( have_posts() ) : 
+			ob_start( );
+			?>
+          
+	  
+<section class="tertiary">
+
+		<ul class="small-block-grid-2 large-block-grid-3">
 
 
-	<?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+<?php
+						 while ( have_posts() ) : the_post(); 
+					
+$webinar_date = get_field( "webinar_date" );
+$webinar_registration_link = get_field_object("webinar_registration_link");
+$todayis = date("Ymd");
+$showdate = DateTime::createFromFormat('Ymd', $webinar_date);
+if($webinar_date >= $todayis){
+	$count++
+					?>
+						
+					
+					<li>
+					<article>
 
-	<?php endif; // End have_posts() check. ?>
+						<?php the_post_thumbnail('small-thumb');?>
 
-	<?php /* Display navigation to next/previous pages when applicable */ ?>
-	<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-		</nav>
-	<?php } ?>
+						<header> 
+							<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+							<p>
+                            <?php echo $showdate->format('d F, Y');; ?><br />
+                            <a href="<?php the_field('webinar_registration_link'); ?>">Register Now</a></p>
+						</header>
+
+						
+
+					</article>
+
+					</li>
+
+					<?php 
+					} //check if in date range
+					endwhile; 
+					$upcomingwebinars = ob_get_clean( );
+					if($count > 0){
+						echo $upcomingwebinars;	
+						}
+					
+					
+					?>
+                   	<?php wp_reset_query(); ?>
+
+		</ul>
+
+		<h6 class="readmore"><a href="#">See More Webinars &raquo;</a></h6>
+
+	</section>
+
+	
+					<?php endif;?>
+
+
+
+<?php 	$args = array('post_type' => array( 'Webinars' ));
+		$query = new WP_Query( $args );
+		$count = 0;
+			if ( have_posts() ) : 
+			ob_start( );
+			?>
+            
+<h1 class="section-title">
+	<span>Past Webinars</span>
+</h1>
+	  
+<section class="tertiary">
+
+		<ul class="small-block-grid-2 large-block-grid-3">
+
+
+
+
+<?php
+						 while ( have_posts() ) : the_post(); 
+					
+$webinar_date = get_field( "webinar_date" );
+$webinar_registration_link = get_field_object("webinar_registration_link");
+$todayis = date("Ymd");
+$showdate = DateTime::createFromFormat('Ymd', $webinar_date);
+if($webinar_date < $todayis){
+	$count++
+					?>
+						
+					
+					<li>
+					<article>
+
+						<?php the_post_thumbnail('small-thumb');?>
+
+						<header> 
+							<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+							<p>
+                            <?php echo $showdate->format('d F, Y');; ?><br />
+                            <a href="<?php the_field('webinar_registration_link'); ?>">Register Now</a></p>
+						</header>
+
+						
+
+					</article>
+
+					</li>
+
+					<?php 
+					} //check if in date range
+					endwhile; 
+					$pastwebinars = ob_get_clean( );
+					if($count > 0){
+						echo $pastwebinars;	
+						}
+					
+					
+					?>
+                   	<?php wp_reset_query(); ?>
+
+		</ul>
+
+		<h6 class="readmore"><a href="#">See More Webinars &raquo;</a></h6>
+
+	</section>
+
+	
+					<?php endif;?>
+
+
 
 	</div>
+					<?php rewind_posts(); ?>
+
+
+
+
+
 	<?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
---
+++ wp-content/themes/eschoolnews/single-ercs.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+<div class="row top">
+	<div class="small-12 large-8 columns" role="main">
+
+				<?php if( is_singular( array('ercs','whitepapers','webinars','special-reports') )) {?>
+				
+				<?php $post_type = get_post_type_object( get_post_type($post) );
+				
+				echo '<span class="flag content">';
+				echo '<a href="' . site_url('/') . get_post_type( get_the_ID() ) . '">';
+				echo $post_type->labels->singular_name; 
+				echo '</a></span>'; ?>
+				
+				<?php }?>
+
+	<?php do_action( 'foundationpress_before_content' ); ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<header>
+				<p class="date"><?php the_time('F j, Y'); ?></p>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<p class="author">By <?php the_author(); ?></p>
+			
+			<?php get_template_part('parts/social'); ?>
+			 </header>
+			
+
+			<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
+			<div class="entry-content">
+
+			<?php if ( has_post_thumbnail() ) : ?>
+				<div class="row">
+					<div class="column">
+						<?php the_post_thumbnail( '', array('class' => 'th') ); ?>
+					</div>
+				</div>
+			<?php endif; ?>
+
+			<?php the_content(); ?>
+			</div>
+
+			<?php get_template_part('parts/social'); ?>
+			
+
+				<div class="author-bio row">
+					<div class="hide-for-small-only large-2 columns author-avatar small-text-center">
+						<?php echo get_avatar($post->post_author, 50); ?>
+					</div>
+					<div class="small-12 large-10 columns author-bio-text">
+						<h5 class="left small-text-center"><strong>About the Author:</strong> <?php echo the_author_posts_link(); ?></h5>
+
+							<ul class="icons right">
+								<?php 
+									$rss_url = get_the_author_meta( 'rss_url' );
+									if ( $rss_url && $rss_url != '' ) {
+										echo '<li class="rss left"><a href="' . esc_url($rss_url) . '"><i class="fi-rss"></i></a></li>';
+									}
+													
+									$google_profile = get_the_author_meta( 'google_profile' );
+									if ( $google_profile && $google_profile != '' ) {
+										echo '<li class="google left"><a href="' . esc_url($google_profile) . '" rel="author"><i class="fi-social-google-plus medium"></i></a></li>';
+									}
+													
+									$twitter_profile = get_the_author_meta( 'twitter_profile' );
+									if ( $twitter_profile && $twitter_profile != '' ) {
+										echo '<li class="twitter left"><a href="' . esc_url($twitter_profile) . '"><i class="fi-social-twitter medium"></i></a></li>';
+									}
+													
+									$facebook_profile = get_the_author_meta( 'facebook_profile' );
+									if ( $facebook_profile && $facebook_profile != '' ) {
+										echo '<li class="facebook left"><a href="' . esc_url($facebook_profile) . '"><i class="fi-social-facebook medium"></i></a></li>';
+									}
+													
+									$linkedin_profile = get_the_author_meta( 'linkedin_profile' );
+									if ( $linkedin_profile && $linkedin_profile != '' ) {
+									       echo '<li class="linkedin left"><a href="' . esc_url($linkedin_profile) . '"><i class="fi-social-linkedin medium"></i></a></li>';
+									}
+								?>
+							</ul>
+
+						<p><?php echo get_the_author_meta('description'); ?></p>
+					</div>
+				</div>
+
+				<footer>
+
+				<?php //wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+			<?php do_action( 'foundationpress_post_before_comments' ); ?>
+			<?php //comments_template(); ?>
+			<?php do_action( 'foundationpress_post_after_comments' ); ?>
+		</article>
+	<?php endwhile;?>
+
+	<?php do_action( 'foundationpress_after_content' ); ?>
+
+	</div>
+	
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
