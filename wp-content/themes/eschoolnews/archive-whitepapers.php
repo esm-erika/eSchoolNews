a1ad6d9a8a639437c56efe41d5e7bf852b10da2b
vincentcarlson <vcarlson@eschoolmedia.com>
2015-09-30 14:43:20 -0400

    WP archive
    Automated based on cats.

--- wp-content/themes/eschoolnews/archive-whitepapers.php
+++ wp-content/themes/eschoolnews/archive-whitepapers.php
@@ -44,9 +44,10 @@ get_header(); ?>
 		  <li class="tab-title active" role="presentation"><a href="#panel1" role="tab" tabindex="0" aria-selected="true" aria-controls="panel1">All White Papers</a></li>
 
 <?php 
-		/* 
+		 
  	     $args = array(
          	  'orderby' => 'name',
+			  'echo' => 0,
 	          'show_count' => 0,
         	  'pad_counts' => 0,
 	          'hierarchical' => 1,
@@ -56,22 +57,29 @@ get_header(); ?>
 			  
         	);
 
-	     wp_list_categories( $args );
-*/
+$tabsconstruct = wp_list_categories( $args );
+//  subject_categories/curriculum/" >Curriculum,
+$replacethis = '/(<li[^>]*>)|(<\/a[^>]*>)|(<\/l[^>]*>)|(<a[^>]*subject_categories\/)/';
+$tabsconstruct = preg_replace($replacethis, ',', $tabsconstruct);
+$tabsconstruct = preg_replace('/\/" >/', '~', $tabsconstruct);
+
+$tabsconstruct = explode(",", $tabsconstruct);
+
+$pannel=2;
+$tabindex=1;
+foreach ($tabsconstruct as $key => $value) {
+	if (preg_match('/\S/', $value)){
+		$pos = strpos($value, '~');
+		$catname =substr( $value , $pos+1);
+		echo '<li class="tab-title" role="presentation"><a href="#panel'.$pannel.'" role="tab" tabindex="'.$tabindex.'" aria-selected="false" aria-controls="panel'.$pannel.'">'.$catname.'</a></li>';
+		$pannel++;$tabindex++;
+	}
+	
+}
+
  ?>
-
-
-
-		  
-         <?php /* <li class="tab-title" role="presentation"><a href="#panel2" role="tab" tabindex="0" aria-selected="false" aria-controls="panel2">Curriculum</a></li>
-		  <li class="tab-title" role="presentation"><a href="#panel3" role="tab" tabindex="0" aria-selected="false" aria-controls="panel3">Digital</a></li>
-		  <li class="tab-title" role="presentation"><a href="#panel4" role="tab" tabindex="0" aria-selected="false" aria-controls="panel4">Mobile &amp; Online Learning</a></li>
-		  <li class="tab-title" role="presentation"><a href="#panel5" role="tab" tabindex="0" aria-selected="false" aria-controls="panel5">Technologies</a></li> */ ?>
-          
-          
-		</ul>
-
-		<div class="tabs-content">
+	</ul>
+	<div class="tabs-content">
         
 <?php 
 /*
@@ -110,9 +118,55 @@ register_taxonomy("subject_categories", array("whitepapers"), array("hierarchica
 		
 
 		  </section>
+<?php
+$pannel=2;
+$tabindex=1;
+foreach ($tabsconstruct as $key => $value) {
+	if (preg_match('/\S/', $value)){
+		$pos = strpos($value, '~');
+		$catname = substr( $value , $pos+1);	
+		$catslug = substr( $value,0, $pos);	
+		
+echo '<section role="tabpanel" aria-hidden="true" class="content" id="panel'.$pannel.'"><h3>'.$catname.'</h3>'?>
+		    <ul class="medium-block-grid-2">
+		    <?php
+				// The Query
+				$args2 = array(
+					//'post_type' => 'whitepapers',
+					'orderby' => 'rand',
+					'tax_query' => array(
+						array(
 
-        
-        
+							'taxonomy' => 'subject_categories',
+							'field' => 'slug',
+							'terms' => $catslug,
+
+							),
+
+						),
+
+					);
+
+				$query = new WP_Query( $args2 ); ?>
+
+
+				<?php // The Loop
+				 while ( $query->have_posts() ) :
+					$query->the_post(); ?>
+
+				<li><?php the_title(); ?></li>
+					
+					<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+
+		   </ul>
+		  </section>
+<?php
+		$pannel++;$tabindex++;
+	}
+	
+}        
+?>        
 		  <?php /*<section role="tabpanel" aria-hidden="false" class="content active" id="panel1">
 		 
 		    <h3>All White Papers</h3>
