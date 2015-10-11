aab9c2e90450451756d24ba041177262f7ecee3d
vincentcarlson <vcarlson@eschoolmedia.com>
2015-10-11 17:40:01 -0400

    Webinar Functions

--- wp-content/themes/eschoolnews/library/options-registration.php
+++ wp-content/themes/eschoolnews/library/options-registration.php
@@ -126,9 +126,297 @@ echo $data_field_name1; ?>" value="<?php echo $opt_val1; ?>" ><br>
 </p>
 
 </form>
-</div>
+
+<h2>Automated Webinar Category Move</h2>
+
+<?php
+    // variables for the field and option names 
+    $webinar_from = 'esm_webinar_from_form'; 
+    $esm_webinar_from = 'esm_webinar_from_form_submit';
+    $data_webinar_from = 'esm_webinar_from_form';
+	
+    $webinar_to = 'esm_webinar_to_form'; 
+    $esm_webinar_to = 'esm_webinar_to_form_submit';
+    $data_webinar_to = 'esm_webinar_to_form';
+    
+	// Read in existing option value from database
+    $webinar_from_val = get_option( $webinar_from );
+	$webinar_to_val = get_option( $webinar_to );
+	
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+$updated = 0;	
+    if( isset($_POST[ $esm_webinar_from ]) && $_POST[ $esm_webinar_from ] == 'Y' ) {
+        // Read their posted value
+        $webinar_from_val = $_POST[ $data_webinar_from ];
+
+        // Save the posted value in the database
+        update_option( $webinar_from, $webinar_from_val );
+
+        // Put an settings updated message on the screen
+		
+		
+        // Read their posted value
+        $webinar_to_val = $_POST[ $data_webinar_to ];
+
+        // Save the posted value in the database
+        update_option( $webinar_to, $webinar_to_val );
+
+        // Put an settings updated message on the screen
+		
+		$updated = 1;
+
+    }
+    
+if($updatedcategories == "1"){
+echo '<div class="updated"><p><strong>Category Settings saved</strong></p></div>';
+}
+
+
+
+    // Now display the settings editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+    echo "<h3>Field Settings</h3>";
+
+    // settings form
+    ?>
+
+<form name="form1" method="post" action="">
+<p>
+Select the category that posts are to move from and to in the Webinar Post Type.
+<p>
+<?php
+
+	if($webinar_from_val >0){
+		$term = get_term( $webinar_from_val, 'status-webinars' );
+		
+		$from_none_value = $webinar_from_val;
+		
+		$from_option_none = $term->name;
+	} else {
+		$from_none_value = -1; $from_option_none = 'Select category';
+	}
+ $webinar_from_args = array(
+	
+	'show_option_none' => $from_option_none,
+	'option_none_value'  => $from_none_value,
+	'orderby'            => 'ID', 
+	'order'              => 'ASC',
+	'hide_empty'         => 0, 
+	'selected'           => 0,
+	'hierarchical'       => 0, 
+	'name'               => $data_webinar_from,
+	'taxonomy'           => 'status-webinars',
+	'hide_if_empty'      => false,
+	'value_field'	     => 'term_id',	
+	);	
+	
+	if($webinar_to_val >0){
+		$term = get_term( $webinar_to_val, 'status-webinars' );
+		
+		$to_none_value = $webinar_to_val;
+		
+		$to_option_none = $term->name;
+	} else {
+		$to_none_value = -1; $to_option_none = 'Select category';
+	}
+
+
+
+ $webinar_to_args = array(
+	
+	'show_option_none' => $to_option_none,
+	'option_none_value'  => $to_none_value,
+	'orderby'            => 'ID', 
+	'order'              => 'ASC',
+	'hide_empty'         => 0, 
+	'selected'           => 0,
+	'hierarchical'       => 0, 
+	'name'               => $data_webinar_to,
+	'taxonomy'           => 'status-webinars',
+	'hide_if_empty'      => false,
+	'value_field'	     => 'term_id',	
+	);
+ ?>
+
+
+<input type="hidden" name="<?php echo $esm_webinar_from; ?>" value="Y">
+ <label style="width:300px" for="<?php echo $data_webinar_from; ?>"> Upcoming Webinar Category:</label>
+<?php wp_dropdown_categories( $webinar_from_args ); ?>
+<br>
+
+<label style="width:300px" for="<?php 
+echo $data_webinar_to; ?>">Webinar Archive Category:</label>
+
+<?php wp_dropdown_categories( $webinar_to_args ); ?>
+</p>
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="Save Category Changes" />
+</p>
+
+</form>
+
+
+
+
+
 
 
 <?php 
+	function esm_movecats() {
+
+
+global $wpdb;
+
+    echo "<h3>Moved Webinar Items:</h3>";
+	// Read in existing option value from database
+    $webinar_from_val = get_option( 'esm_webinar_from_form' );
+	$webinar_to_val = get_option( 'esm_webinar_to_form' );
+
+$todaydatenum = date('Ymd',time() );
+
+$args = array( 'taxonomy' => 'status-webinars', 'post_type' => 'webinars', 'posts_per_page' => 1000 );
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+  
+  
+  $postid = get_the_ID() ;
+  $webdate = get_post_meta($postid, 'webinar_date', true);
+
+// An array of IDs of categories we want this post to have.
+$cat_ids = array( $webinar_to_val );
+
+$cat_ids = array_map( 'intval', $cat_ids );
+$cat_ids = array_unique( $cat_ids );
+
+
+	if($webdate < $todaydatenum){
+		echo get_the_title( $postid ).'<hr>';
+		wp_set_object_terms( $postid, $cat_ids, 'status-webinars' );
+		
+	}
+
+
+endwhile;
+
+
+//foreach ($query2 as $item){
+ //echo get_the_title( $ID );	
+	
+//	echo"<pre>";
+//	print_r($item);
+///	echo"</pre>";
+	
+//	$webdate = get_post_meta($post->ID, 'webinar_date', true);
+	//echo '<h1>'.$webdate.'</h1>';
+	
+	
+	//loop through items and move
+	
+	//remove from category 1
+	
+	//change category count
+	
+	//add to category 2 
+	
+	//update category count
+		
+	
+//} 
+
+
+/*
+$my_text = "/images";  // What I'm searching for
+$my_category = '8';    // The category to change it to
+
+
+// Get the ID field (which is WordPress's post
+// number) from any posts that have your text:
+$query = "SELECT ID FROM wp_posts WHERE post_content LIKE '%$my_text%'"; 
+
+// Take those results and go through them:
+$result = mysql_query($query) or die(mysql_error());
+
+// While there are results...
+while($row = mysql_fetch_array($result))
+{
+// Verify what we're doing -- changing post
+// number such-and-such...
+$thisPostHasIt = $row['ID'];
+echo "<p>Row " . $row['ID'] . " contains it, so...<br />";
+
+// In the wp_term_relationships table,
+// update the category number ("term_taxonomy_id")
+// with the category number you specified -- but only
+// in one of the "result" rows.
+// We look for "object_id" to equal one of those
+// rows. (The object_id field refers to the WordPress
+// post number, just as the ID field did. Why two
+// different names? Who knows?)
+
+mysql_query("UPDATE wp_term_relationships SET term_taxonomy_id='$my_category' WHERE object_id = '$thisPostHasIt'");
+
+// And tell us about it:
+echo "Changing post number " . $thisPostHasIt . " to category number ". $my_category . "</p>";
+}
+echo "<p><b>All done!</b></p>";
+		
+*/		
+		
+		
+	}
+	//register_activation_hook(__FILE__, 'esm_movecats_activation');
+	    
+if( isset($_POST[ 'ChangeMove' ]) && $_POST[ 'ChangeMove'] == 'Y' ) {
+		
+		if( isset($_POST[ 'Submit' ]) && $_POST[ 'Submit' ] == 'Activate' ) {
+		
+			$stamp = mktime(0, 0, 0);
+			wp_schedule_event($stamp, 'daily', 'esm_daily_movecat');
+		}elseif( isset($_POST[ 'Submit' ]) && $_POST[ 'Submit' ] == 'Deactivate' ) {
+		
+			wp_clear_scheduled_hook('esm_daily_movecat');
+		}elseif( isset($_POST[ 'Submit' ]) && $_POST[ 'Submit' ] == 'Run Move Now' ) {
+		
+			esm_movecats();
+		}				
+	
+}
+	
+	
+	$CheckSchedule = wp_next_scheduled( 'esm_daily_movecat' );
+
+
+?>
+<p></p>
+<?php
+//$cron_jobs = get_option( 'cron' );
+//var_dump($_POST);
+//var_dump($cron_jobs);
+echo "<h3>Schedule Setting</h3>";
+
+ if($CheckSchedule){ echo '<p> Scheduled to run at '.gmdate("d/m/Y  H:i:s a", $CheckSchedule).'</p>';}else{echo'<p>Current Status: Deactived</p>';} ?>
+
+<p class="submit">
+<form name="form2" method="post" action="">
+<input type="hidden" name="ChangeMove" value="Y">
+<input type="submit" name="Submit" class="button-primary" value="Activate" />
+
+
+<input type="submit" name="Submit" class="button-primary" value="Deactivate" />
+
+
+<input type="submit" name="Submit" class="button-primary" value="Run Move Now" />
+</form>
+
+
+</p>
+
+</div>
+<?php
 }
 ?>
