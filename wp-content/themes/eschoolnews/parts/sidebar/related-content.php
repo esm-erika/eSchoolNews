1a591a0e96e45da03d8634c93675d405ed58b26f
vincentcarlson <vcarlson@eschoolmedia.com>
2015-10-21 15:43:25 -0400

    Related Posts, commit me in now if it does not work

--- wp-content/themes/eschoolnews/parts/sidebar/related-content.php
+++ wp-content/themes/eschoolnews/parts/sidebar/related-content.php
@@ -1,26 +1,41 @@
-<?php 
-    $orig_post = $post;
-    global $post;
-    $tags = wp_get_post_tags($post->ID);
-     
-    if ($tags) {
-    $tag_ids = array();
-    foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-	
-	$args=array(
-    'tag__in' => $tag_ids,
-    'post__not_in' => array($post->ID),
-    'posts_per_page'=>4, // Number of related posts to display.
-    'ignore_sticky_posts'=>1,
-	'post_type' => array( 'whitepapers' ,'ercs' ,'webinars' ,'special-reports','post' )    );
-  
-    $my_query = new wp_query( $args );
-	
-if ( have_posts() ) {	
-?>
 <article>
 <section>
 <h3 class="section-title"><span>Related posts</span></h3>	
+<?php 
+
+//$args = array( 'category__in => 6870, 'tag__in' => array( 'tag1', 'tag2' ), 'showposts' => 2 );
+
+    $orig_post = $post;
+    global $post;
+    $tags = wp_get_post_tags($post->ID);
+    $tag_ids = array();
+	foreach($tags as $individual_tag){ $tag_ids[] = $individual_tag->term_id;}
+	$cats = wp_get_post_categories($post->ID);
+    $cat_ids = array();
+    foreach($cats as $individual_cat){ $cat_ids[] = $individual_cat->term_id;}
+
+	$args=array(
+    'post__not_in' => array($post->ID),
+    'posts_per_page'=>5, // Number of related posts to display.
+    'ignore_sticky_posts'=>1,
+	'post_type' => array( 'whitepapers' ,'ercs' ,'webinars' ,'special-reports','post' ),  
+	'tax_query' => array(
+	    'relation' => 'OR',
+		array(
+			'category__in ' => $cat_ids
+		),
+		array(
+			'tag__in' => $tag_ids
+		)
+	)
+	 
+	 );
+
+
+    $my_query = new wp_query( $args );
+//if ( have_posts() ) {	
+?>
+
 <?php
     while( $my_query->have_posts() ) {
       $my_query->the_post();
@@ -53,12 +68,11 @@ if ( have_posts() ) {
 	 
 
 	 
-    }
 ?>	
 </section>
 </article>
 <?php	
-	}
+//	}
     $post = $orig_post;
     wp_reset_query();
 ?>
