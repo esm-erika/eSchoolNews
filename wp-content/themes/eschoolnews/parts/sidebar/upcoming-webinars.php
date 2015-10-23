85556cf4e51ef0691ebcdbe48b4caaf2ad83a845
Erika Lewis <me.erikawithak@gmail.com>
2015-10-23 14:16:53 -0400

    update to date

--- wp-content/themes/eschoolnews/parts/sidebar/upcoming-webinars.php
+++ wp-content/themes/eschoolnews/parts/sidebar/upcoming-webinars.php
@@ -30,7 +30,7 @@
 					<div class="large-12 columns">
 						<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
 						<p class="date"><?php 
-				$showdate = DateTime::createFromFormat('Ymd', get_field('webinar_date'));
+				$showdate = DateTime::createFromFormat('Ymd', get_field('event_date'));
 				if($showdate){ echo $showdate -> format('F d, Y');} ?></p>
                          
                         
