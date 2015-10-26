0c39932abfae4916c05f95124c890c5d2a7f873c
Erika Lewis <me.erikawithak@gmail.com>
2015-10-26 17:16:04 -0400

    added clearfix to flags

--- wp-content/themes/eschoolnews/assets/javascript/social-sharing.js
+++ wp-content/themes/eschoolnews/assets/javascript/social-sharing.js
@@ -1,7 +1,7 @@
-========> Twitter script:
+//========> Twitter script:
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-========> Facebook HTML5 script:
+//========> Facebook HTML5 script:
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -10,10 +10,10 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-========> Buffer script: 
+//========> Buffer script: 
 <script type="text/javascript" src="https://d389zggrogs7qo.cloudfront.net/js/button.js"></script>
 
-========> Google+ script:
+//========> Google+ script:
 <script type="text/javascript">
 (function() {
   var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
