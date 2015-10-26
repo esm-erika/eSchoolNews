06ac34dea7fa215ba4d19da119d4b2df9e08eff2
Erika Lewis <me.erikawithak@gmail.com>
2015-10-26 16:00:24 -0400

    update social media links in header

--- wp-content/themes/eschoolnews/header.php
+++ wp-content/themes/eschoolnews/header.php
@@ -34,7 +34,29 @@
   <!-- slick-theme.css default styling -->
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/slick/slick-theme.css"/>
 
+  <!-- //========> Twitter script: -->
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
+  <!-- //========> Facebook HTML5 script: -->
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- //========> Buffer script:  -->
+  <script type="text/javascript" src="https://d389zggrogs7qo.cloudfront.net/js/button.js"></script>
+
+  <!-- //========> Google+ script: -->
+  <script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/platform.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+  </script>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -93,7 +115,7 @@
       <div class="social right">
         <a href="http://www.twitter.com/eschoolnews" target="new" class="right"><i class="fi-social-twitter medium"></i></a>
         <a href="http://www.facebook.com/eschoolnews/" target="new" class="right"><i class="fi-social-facebook medium"></i></a>
-
+        <a href="mailto:webmaster@eschoolnews.com"><i class="fi-mail medium"></i></a>
       </div>
 
       <a href="#" data-dropdown="drop2" aria-controls="drop2" aria-expanded="false" class="search"> <i class="fi-magnifying-glass"></i> Search</a>
