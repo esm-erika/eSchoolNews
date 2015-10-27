158f8d3f65212f08a81ad68c942bac0b3dde1df3
Erika Lewis <me.erikawithak@gmail.com>
2015-10-27 11:41:39 -0400

    made date small caps

--- wp-content/themes/eschoolnews/parts/sidebar/professional-development.php
+++ wp-content/themes/eschoolnews/parts/sidebar/professional-development.php
@@ -67,7 +67,7 @@ $query2 = new WP_Query( $events ); ?>
 
 
 <li>
-		<div><?php 
+		<div class="small-caps"><?php 
 		$showdate = DateTime::createFromFormat('Ymd', get_field('event_date'));
 		if($showdate){ echo $showdate -> format('F d, Y');} ?></div>
 		<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
