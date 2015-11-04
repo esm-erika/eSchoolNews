7c4227aa9511f96986242b5a691b9df6e0f5d2a4
Erika Lewis <me.erikawithak@gmail.com>
2015-11-04 15:06:30 -0500

    fixing flag

--- wp-content/themes/eschoolnews/parts/flags.php
+++ wp-content/themes/eschoolnews/parts/flags.php
@@ -1,12 +1,23 @@
 	<?php 
 
+	$post_type = get_post_type( $post->ID ); 
+
+	if ($post_type == 'post') {
+
+		echo '<span class="flag content">';
+		echo '<a href="' . site_url('/') . get_post_type( get_the_ID() ) . '">';
+		echo 'News'; 
+		echo '</a></span>';
+	
+	} elseif {
+
 		$post_type = get_post_type_object( get_post_type($post) );
 
 		echo '<span class="flag content">';
 		echo '<a href="' . site_url('/') . get_post_type( get_the_ID() ) . '">';
 		echo $post_type->labels->singular_name; 
-		echo '</a></span>'; 
+		echo '</a></span>';
 
-	  ?>
+	} ?>
 
-	 <!-- $post_type = get_post_type( $post->ID ); if ($post_type == 'project') echo 'Work'; -->
+	 <!--  -->
