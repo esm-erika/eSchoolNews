181ad3731a48ff1bb70c59b4381a61cb725fa85f
Erika Lewis <me.erikawithak@gmail.com>
2015-11-05 12:25:51 -0500

    update to flags code

--- wp-content/themes/eschoolnews/parts/flags.php
+++ wp-content/themes/eschoolnews/parts/flags.php
@@ -5,7 +5,7 @@
 	if ($post_type == 'post') {
 
 		echo '<span class="flag content">';
-		echo '<a href="' . site_url('/') . the_permalink() . '">';
+		echo '<a href="' . site_url('/') . get_the_permalink() . '">';
 		echo 'News'; 
 		echo '</a></span>';
 	
