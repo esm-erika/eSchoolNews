0e359190b8fccc83fc80e4e591a0077edcef5af2
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-06 11:27:02 -0500

    changes sidebar

--- wp-content/themes/eschoolnews/parts/sidebar/astc.php
+++ wp-content/themes/eschoolnews/parts/sidebar/astc.php
@@ -10,24 +10,23 @@
 	echo '<!-- '.$astc. ' -->';
 	 if($astc > 1){
 
-?>    
-    
-    <article>
-	<section>
-	<h4>Table of Contents</h4>	
-        <ul>
-         <?php
-
 			$e = 1; $query5 = new WP_Query();$query5->query('cat='.$astc);
 			while ($query5->have_posts()) : $query5->the_post(); 
+			if(e ==1){ ?>
+				
+            <article>
+            <section>
+            <h4>Table of Contents</h4>	
+                <ul>				
+				
+			<?php	}
 			$toc = $toc . '<li><a href="'. get_permalink(). '?ast='.$astused.'&astc='.$astc.'" rel="bookmark">'. get_the_title() .'</a></li>';
 			
 			 $e++; endwhile; wp_reset_query(); 
 
-			?>
-
+	if(e ==1){ ?>
         </ul>	
-	</div>
 	</section>
 	</article> 
-<?php } ?>
+<?php } 
+} ?>
