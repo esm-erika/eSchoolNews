ba92115c04fd7291beda1c9efa69d71b181350f8
Erika Lewis <me.erikawithak@gmail.com>
2015-11-08 16:59:03 -0500

    update to conferences section

--- wp-content/themes/eschoolnews/parts/sidebar/conferences.php
+++ wp-content/themes/eschoolnews/parts/sidebar/conferences.php
@@ -1,16 +1,17 @@
 <article class="conferences">
 	<h4>Conferences</h4>
 
-<a href="<?php site_url(); ?>/tag/aasa">AASA</a></li> 
-<a href="<?php site_url(); ?>/tag/alas">ALAS</a>
-<a href="<?php site_url(); ?>/tag/ascd">ASCD</a>
-<a href="<?php site_url(); ?>/tag/blc">BLC</a>
-<a href="<?php site_url(); ?>/tag/cosn">COSN</a>
-<a href="<?php site_url(); ?>/tag/cue">CUE</a>
-<a href="<?php site_url(); ?>/tag/fetc">FETC</a>
-<a href="<?php site_url(); ?>/tag/infocomm">InfoComm</a>
-<a href="<?php site_url(); ?>/tag/iste">ISTE</a>
-<a href="<?php site_url(); ?>/tag/nsba">NSBA<a/>
-<a href="<?php site_url(); ?>/tag/tcea">TCEA</a>
-
+<ul class="small-block-grid-1 medium-block-grid-2">
+	<li><a href="<?php site_url(); ?>/tag/aasa">AASA</a></li> 
+	<li><a href="<?php site_url(); ?>/tag/alas">ALAS</a></li>
+	<li><a href="<?php site_url(); ?>/tag/ascd">ASCD</a></li>
+	<li><a href="<?php site_url(); ?>/tag/blc">BLC</a></li>
+	<li><a href="<?php site_url(); ?>/tag/cosn">COSN</a></li>
+	<li><a href="<?php site_url(); ?>/tag/cue">CUE</a></li>
+	<li><a href="<?php site_url(); ?>/tag/fetc">FETC</a></li>
+	<li><a href="<?php site_url(); ?>/tag/infocomm">InfoComm</a></li>
+	<li><a href="<?php site_url(); ?>/tag/iste">ISTE</a></li>
+	<li><a href="<?php site_url(); ?>/tag/nsba">NSBA<a/></li>
+	<li><a href="<?php site_url(); ?>/tag/tcea">TCEA</a></li>
+</ul>
 </article>
