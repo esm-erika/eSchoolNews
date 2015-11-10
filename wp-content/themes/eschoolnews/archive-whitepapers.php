32d224bcd783a695ec44824c7f26469f3a3349e5
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-10 16:05:06 -0500

    whitepaper

--- wp-content/themes/eschoolnews/archive-whitepapers.php
+++ wp-content/themes/eschoolnews/archive-whitepapers.php
@@ -58,7 +58,7 @@ get_header(); ?>
 				 while ( $query->have_posts() ) :
 					$query->the_post(); ?>
 
-					<div class="row  <?php $term_list = wp_get_post_terms($post->ID, 'my_taxonomy', array("fields" => "all"));
+					<div class="row  <?php $term_list = wp_get_post_terms($post->ID, 'Subjects', array("fields" => "all"));
 echo "<!-- ";
 print_r($term_list);
 echo " --> ";
