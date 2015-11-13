bcc5ceee661dbd613b02bd3ac684f16b93a054cf
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-13 16:33:29 -0500

    single view change for erc article

--- wp-content/themes/eschoolnews/library/bucket.php
+++ wp-content/themes/eschoolnews/library/bucket.php
@@ -8,15 +8,15 @@
  */
 
 function custom_excerpt_length( $length ) {
-	return 20;
+	return 30;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 
-function new_excerpt_more( $more ) {
-	return '';
-}
-add_filter('excerpt_more', 'new_excerpt_more');  
+//function new_excerpt_more( $more ) {
+//	return '';
+//}
+//add_filter('excerpt_more', 'new_excerpt_more');  
 
 
 
