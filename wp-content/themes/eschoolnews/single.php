7446d283dfcbf3fa950774d9d23332487f42c082
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-13 15:21:06 -0500

    content registration check

--- wp-content/themes/eschoolnews/single.php
+++ wp-content/themes/eschoolnews/single.php
@@ -70,9 +70,49 @@ include('single-coa.php');
 
     			
     		endif;
-			 ?>
 
-			 <?php the_content(); 
+			if (esm_is_user_logged_in()){
+				$showpagecontent = 1;
+			} else { 
+				$reg_requirement=get_post_meta($post->ID, 'registration_requirement_for_content', $single = true); /*	0 : Default,  1 : Required,  2 : Not Required */
+				if($reg_requirement == 1){
+					$showpagecontent = 0;
+				}
+			}
+
+				if($showpagecontent == 0){
+				the_excerpt()
+			?>
+
+<div style="border:#CCCCCC solid 1px; padding:10px;">
+<form action="<?php echo get_option('home'); ?>/wp-login.php?wpe-login=esminc" method="post">
+<p><strong>Free registration required to view this resource.</strong><br />
+<br />
+Register today and receive free access to all our news and resources and the ability to customize your news by topic with My eSchool News.<br /><br />
+<a href="<?php echo get_option('home'); ?>/registration/?action=register&redirect_to=<?php echo urlencode(get_permalink()); ?><?php if ( defined($_GET['astc'])){ echo '&astc='.$_GET['astc']; }?><?php if ( defined($_GET['ast'])){ echo '&ast='.$_GET['ast']; }?>" style="text-decoration:underline;"><strong>Register now.</strong></a>
+<br />
+<br />
+Already a member? Log in
+<div>Username: <input type="text" name="log" id="log" value="" /></div>
+<div>Password:&nbsp <input name="pwd" id="pwd" type="password" value="" /></div>
+<input type="submit" name="submit" value="Login" class="button">
+<input name="rememberme" id="rememberme" type="hidden" checked="checked" value="forever">
+<input type="hidden" name="redirect_to" value="<?php echo $_SERVER['REQUEST_URI']; ?>">
+<br />
+<a href="<?php echo get_option('home'); ?>/wp-login.php?action=lostpassword&redirect_to=<?php echo urlencode(get_permalink()); ?>">Lost Password?</a>
+
+</form>	
+</div> <?php
+
+
+
+					
+				} else {
+					 the_content(); 
+				}
+				
+				 
+				 
 			 //wp_link_pages( array ( 'next_or_number' => 'next',
 			 // 'before' => '<div id="page-links">',
 			 // 'after' => '</div>' ) );
