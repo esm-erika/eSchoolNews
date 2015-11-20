06fead5e8057a3a6637d73eed9bd268f81411334
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-20 12:39:00 -0500

    caching header

--- wp-content/themes/eschoolnews/header.php
+++ wp-content/themes/eschoolnews/header.php
@@ -185,16 +185,42 @@ get_template_part( 'library/logged-in-check' );
       </div>
     </div>
 
+<?php 
+//insert cache query
+$box_qt = 'esm_c_header_menu';
+$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
+$local_box_cache = get_transient( $box_q );
+if (false === ($local_box_cache) ){
+	// start code to cache
+		ob_start( );
+		echo '<!-- c -->'; ?>
+
     <?php get_template_part( 'parts/off-canvas-menu' ); ?>
-
-
 
     <?php get_template_part( 'parts/top-bar' ); ?>
 
     <section class="container" role="document">
 
-     <?php do_action( 'foundationpress_after_header' ); ?>
+    <?php do_action( 'foundationpress_after_header' ); ?>
 
+<?php
+		echo '<!-- c '.date(DATE_RFC2822).' -->' ;
+		$local_box_cache = ob_get_clean( );
+	// end the code to cache
+		echo $local_box_cache;
+	//end cache query 
+	
+	if( current_user_can( 'edit_post' ) ) {
+		//you cannot cache it
+	} else {
+		set_transient($box_q ,$local_box_cache, 60 * 10);
+	}
+} else { 
+
+echo $local_box_cache;
+
+}
+?>
 
      <?php get_template_part( 'parts/ads/billboard' ); ?>      
 
