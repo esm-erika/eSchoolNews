87e9a4d8b3e11d27c0fc3199dbda44299de469c9
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-23 13:57:00 -0500

    cache single olddata and ldl

--- wp-content/themes/eschoolnews/single-olddata.php
+++ wp-content/themes/eschoolnews/single-olddata.php
@@ -35,6 +35,25 @@ include('single-coa.php');
 	
 	   } else { ?>
 
+
+<?php 
+//insert cache query
+//name format esm_c_[template name in 5 char]_a[ast]c[astc]c[category]p[post id(if sidebar needs to be unique]t[if a tag page]
+//$cat_name = get_category(get_query_var('cat'))->term_id;
+
+global $astc, $astused;
+$post_id = get_the_ID(); 
+$box_qt = 'esm_c_solddata_a'.$astused."c".$astc.'c'.'p'.$post_id;
+$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
+	
+$local_box_cache = get_transient( $box_q );
+if (false === ($local_box_cache) ){
+
+	// start code to cache
+		ob_start( );
+			echo '<!-- c a'.$astused."c".$astc.'c'.'p'.$post_id.'-->';
+?>
+
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -122,7 +141,24 @@ include('single-coa.php');
 			<?php //comments_template(); ?>
 			<?php do_action( 'foundationpress_post_after_comments' ); ?>
 		</article>
+<?php
+			echo '<!-- c '.date(DATE_RFC2822).' -->' ;
+		$local_box_cache = ob_get_clean( );
+	// end the code to cache
+		echo $local_box_cache;
+	//end cache query 
+	
+	if( current_user_can( 'edit_post' ) ) {
+		//you cannot cache it
+	} else {
+		set_transient($box_q ,$local_box_cache, 60 * 10);
+	}
+} else { 
 
+echo $local_box_cache;
+
+}
+?>
 
 <?php } ?>
 
