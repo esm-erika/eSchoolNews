75cd2259e30c71fb05391cacb6bf2e16841d7da2
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-25 17:52:33 -0500

    view all

--- wp-content/themes/eschoolnews/parts/top-stories/layout-1x3_noad.php
+++ wp-content/themes/eschoolnews/parts/top-stories/layout-1x3_noad.php
@@ -83,9 +83,7 @@ $catplug = $current_category->slug;
 	if (is_category()) {
 
 ?>
-	<?php /*<a href="<?php site_url(); ?>/<?php echo $catplug; ?>/view-all-<?php echo $catplug; ?>">See All <?php single_cat_title(); ?> Top Stories &raquo;</a> */ ?>
-<a href="<?php current_page_url() ?>/page/2/">See All <?php single_cat_title(); ?> Top Stories &raquo;</a>
-
+	<a href="<?php site_url(); ?>/<?php echo $catplug; ?>/page/2/>">See All <?php single_cat_title(); ?> Top Stories &raquo;</a>
 
 <?php } else { ?>
 
