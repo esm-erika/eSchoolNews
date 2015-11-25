56d97dbd91c8167e13a70472a3d0db77ef1ae2be
vincentcarlson <vcarlson@eschoolmedia.com>
2015-11-25 17:20:25 -0500

    view all

--- wp-content/themes/eschoolnews/category.php
+++ wp-content/themes/eschoolnews/category.php
@@ -150,37 +150,37 @@ echo 'page 2';
 // var_dump( $queried_object );
 //$tag_id = get_query_var('term_taxonomy_id');
 //$post_id = get_the_ID(); 
-$cat_name = get_category(get_query_var('cat'))->term_id;
-global $astc, $astused;
-$box_qt = 'esm_c_catva_a'.$ast."c".$astc.'c'.$cat_name;
-$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
+//$cat_name = get_category(get_query_var('cat'))->term_id;
+//global $astc, $astused;
+//$box_qt = 'esm_c_catva_a'.$ast."c".$astc.'c'.$cat_name;
+//$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
 	
-$local_box_cache = get_transient( $box_q );
-if (false === ($local_box_cache) ){
+//$local_box_cache = get_transient( $box_q );
+//if (false === ($local_box_cache) ){
 
-	// start code to cache
-		ob_start( );
-			echo '<!-- c -->';
+	/// start code to cache
+		//ob_start( );
+	//		echo '<!-- c -->';
 			get_sidebar();
-			echo '<!-- c '.date(DATE_RFC2822).' -->' ;
-		$local_box_cache = ob_get_clean( );
+	//		echo '<!-- c '.date(DATE_RFC2822).' -->' ;
+	//	$local_box_cache = ob_get_clean( );
 	// end the code to cache
-		echo $local_box_cache;
+	//	/echo $local_box_cache;
 	//end cache query 
 	
-	if( current_user_can( 'edit_post' ) ) {
-		//you cannot cache it
-	} else {
-		set_transient($box_q ,$local_box_cache, 60 * 10);
-	}
-} else { 
+	//if( current_user_can( 'edit_post' ) ) {
+		///you cannot cache it
+//	} else {
+	//	set_transient($box_q ,$local_box_cache, 60 * 10);
+	//}
+//} else { 
+//
+//echo $local_box_cache;
 
-echo $local_box_cache;
-
-}
+//}
 ?>
 	</div>
-	<?php //get_footer(); 
+	<?php get_footer(); 
 
 
 
