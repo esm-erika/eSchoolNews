306fbb25dbaebe94b6d3896f718b7eb75df229fb
Erika Lewis <me.erikawithak@gmail.com>
2015-12-09 16:16:31 -0500

    update for contributor information

--- wp-content/themes/eschoolnews/single.php
+++ wp-content/themes/eschoolnews/single.php
@@ -46,7 +46,22 @@ include('single-coa.php');
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<div class="small-caps">By <?php the_author(); ?></div>
+
+				<div class="small-caps">
+					By 
+					<?php  if( get_field('Alt Author Read More Name')) {
+
+						the_field('Alt Author Read More Name');
+
+					} else {
+
+						the_author();
+
+					} ?>
+
+				</div>
+
+
 							<div class="posted-on">Posted on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?></div>		
 
 			
