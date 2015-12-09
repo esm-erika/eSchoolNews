8c14edfad7aa26dd2469ee38efcb9756156dbea5
Erika Lewis <me.erikawithak@gmail.com>
2015-12-09 17:34:42 -0500

    resources sidebar

--- wp-content/themes/eschoolnews/parts/sidebar/resources.php
+++ wp-content/themes/eschoolnews/parts/sidebar/resources.php
@@ -14,7 +14,7 @@ if(is_post_type_archive('webinars' )) {
 
 <article>
 
-	<?php if (is_post_type_archive( array('webinars', 'special-reports', 'ercs')) || is_singular('whitepapers') ) { ?>
+	<?php if (is_singular( array('webinars', 'special-reports', 'ercs', 'whitepapers')) ) { ?>
 	<h4>Other Resources</h4>
 	<?php } else { ?>
 	<h4>Resources</h4>
