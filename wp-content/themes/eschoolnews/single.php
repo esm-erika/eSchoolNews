fc57d5ad98ae05e4e9d0c32a95fc14c174f35c1c
vincentcarlson <vcarlson@eschoolmedia.com>
2015-12-10 12:40:19 -0500

    Reg Check debug

--- wp-content/themes/eschoolnews/single.php
+++ wp-content/themes/eschoolnews/single.php
@@ -91,7 +91,7 @@ include('single-coa.php');
 
 			if (esm_is_user_logged_in()){
 				$showpagecontent = 1; 
-			} else { echo 'esm_is_user_logged_in = '. esm_is_user_logged_in();
+			} else { echo '<!-- esm_is_user_logged_in = '. esm_is_user_logged_in(). ' -->';
 				$reg_requirement=get_post_meta($post->ID, 'registration_requirement_for_content', $single = true); /*	0 : Default,  1 : Required,  2 : Not Required */
 				echo '<!-- reg_requirement = '.$reg_requirement. ' -->';
 				$astcset = $_GET['astc'];
