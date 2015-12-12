f8b55e4784e9b580b4cfe0bb3b37f5bff1b76ba8
vincentcarlson <vcarlson@eschoolmedia.com>
2015-12-12 14:38:07 -0500

    wp page

--- wp-content/themes/eschoolnews/archive-whitepapers.php
+++ wp-content/themes/eschoolnews/archive-whitepapers.php
@@ -28,7 +28,77 @@ get_header();
 
 
 </div>
+<script type="text/javascript">
+jQuery.noConflict()(function ($) { // this was missing for me
+$(document).ready(function(){
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
+		$("h4#MobileTitle").hide();
+		$("h4#TechnologiesTitle").hide();	
+	$("#all").click(function(){
+        $("div.row.all").show();
+		$("h4#AllTitle").show();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
+		$("h4#MobileTitle").hide();
+		$("h4#TechnologiesTitle").hide();		
+		
+    });
 
+	$("#curriculum").click(function(){
+		$("div.row.digital-whitepapers").hide();
+		$("div.row.technologies-whitepapers").hide();
+		$("div.row.mobile-online-whitepapers").hide();
+        $("div.row.curriculum-whitepapers").show();
+		$("h4#AllTitle").hide();
+		$("h4#CurriculumTitle").show();
+		$("h4#DigitalTitle").hide();
+		$("h4#MobileTitle").hide();
+		$("h4#TechnologiesTitle").hide();		
+
+    });
+
+	$("#digital").click(function(){
+        $("div.row.curriculum-whitepapers").hide();
+		$("div.row.technologies-whitepapers").hide();
+		$("div.row.mobile-online-whitepapers").hide();
+		$("div.row.digital-whitepapers").show();
+		$("h4#AllTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").show();
+		$("h4#MobileTitle").hide();
+		$("h4#TechnologiesTitle").hide();		
+
+    });
+
+	$("#mobile").click(function(){
+        $("div.row.curriculum-whitepapers").hide();
+		$("div.row.digital-whitepapers").hide();
+		$("div.row.technologies-whitepapers").hide();
+		$("div.row.mobile-online-whitepapers").show();
+		$("h4#AllTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
+		$("h4#MobileTitle").show();
+		$("h4#TechnologiesTitle").hide();		
+
+    });	
+    
+	$("#technologies").click(function(){
+        $("div.row.curriculum-whitepapers").hide();
+		$("div.row.digital-whitepapers").hide();
+		$("div.row.mobile-online-whitepapers").hide();
+		$("div.row.technologies-whitepapers").show();
+		$("h4#AllTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
+		$("h4#MobileTitle").hide();
+		$("h4#TechnologiesTitle").show();		
+
+    });
+	});
+});
+</script>
 <div class="row">
 	<div class="small-12 medium-12 columns">
 		
@@ -186,73 +256,5 @@ echo $local_box_cache;
 
 
 </div>
-<script type="text/javascript">
-$(document).ready(function(){
-		$("h4#CurriculumTitle").hide();
-		$("h4#DigitalTitle").hide();
-		$("h4#MobileTitle").hide();
-		$("h4#TechnologiesTitle").hide();	
-	$("#all").click(function(){
-        $("div.row.all").show();
-		$("h4#AllTitle").show();
-		$("h4#CurriculumTitle").hide();
-		$("h4#DigitalTitle").hide();
-		$("h4#MobileTitle").hide();
-		$("h4#TechnologiesTitle").hide();		
-		
-    });
 
-	$("#curriculum").click(function(){
-		$("div.row.digital-whitepapers").hide();
-		$("div.row.technologies-whitepapers").hide();
-		$("div.row.mobile-online-whitepapers").hide();
-        $("div.row.curriculum-whitepapers").show();
-		$("h4#AllTitle").hide();
-		$("h4#CurriculumTitle").show();
-		$("h4#DigitalTitle").hide();
-		$("h4#MobileTitle").hide();
-		$("h4#TechnologiesTitle").hide();		
-
-    });
-
-	$("#digital").click(function(){
-        $("div.row.curriculum-whitepapers").hide();
-		$("div.row.technologies-whitepapers").hide();
-		$("div.row.mobile-online-whitepapers").hide();
-		$("div.row.digital-whitepapers").show();
-		$("h4#AllTitle").hide();
-		$("h4#CurriculumTitle").hide();
-		$("h4#DigitalTitle").show();
-		$("h4#MobileTitle").hide();
-		$("h4#TechnologiesTitle").hide();		
-
-    });
-
-	$("#mobile").click(function(){
-        $("div.row.curriculum-whitepapers").hide();
-		$("div.row.digital-whitepapers").hide();
-		$("div.row.technologies-whitepapers").hide();
-		$("div.row.mobile-online-whitepapers").show();
-		$("h4#AllTitle").hide();
-		$("h4#CurriculumTitle").hide();
-		$("h4#DigitalTitle").hide();
-		$("h4#MobileTitle").show();
-		$("h4#TechnologiesTitle").hide();		
-
-    });	
-    
-	$("#technologies").click(function(){
-        $("div.row.curriculum-whitepapers").hide();
-		$("div.row.digital-whitepapers").hide();
-		$("div.row.mobile-online-whitepapers").hide();
-		$("div.row.technologies-whitepapers").show();
-		$("h4#AllTitle").hide();
-		$("h4#CurriculumTitle").hide();
-		$("h4#DigitalTitle").hide();
-		$("h4#MobileTitle").hide();
-		$("h4#TechnologiesTitle").show();		
-
-    });
-});
-</script>
 <?php get_footer(); ?>
