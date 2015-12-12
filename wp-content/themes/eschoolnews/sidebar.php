084130dd65ef58691a1cabf927a383402ab60686
vincentcarlson <vcarlson@eschoolmedia.com>
2015-12-12 10:49:38 -0500

    category page debug

--- wp-content/themes/eschoolnews/sidebar.php
+++ wp-content/themes/eschoolnews/sidebar.php
@@ -13,15 +13,15 @@
 <aside id="sidebar" class="small-12 medium-4 columns">
 
 	<?php 
-echo '- 16 -';
+
 	if ( is_singular('whitepapers') || in_category('leading-the-digital-leap')) {
 
 		echo '';
-echo '- 20 -';
+
 	} else {
 
 		get_template_part( 'parts/ads/embeddedbanner' );
-echo '- 24 -';
+
 	} ?>
 
 	<?php
@@ -29,90 +29,89 @@ echo '- 24 -';
 	?>
 
 	<?php if(is_page('Resources')){
-		get_template_part( 'parts/sidebar/topics' );echo '- 32 -';
+		get_template_part( 'parts/sidebar/topics' );
 	} ?>
 
 	<?php if(is_post_type_archive('events')){
-		get_template_part( 'parts/sidebar/conferences' );echo '- 36 -';
+		get_template_part( 'parts/sidebar/conferences' );
 	} ?>
 
 	<?php 
 	if( is_page('Resources') || is_post_type_archive('events')) { 
-		get_template_part( 'parts/sidebar/upcoming-webinars' );echo '- 41 -';
+		get_template_part( 'parts/sidebar/upcoming-webinars' );
 	} ?>
 
 	<?php
 	if( in_category('leading-the-digital-leap')) { 
-		get_template_part( 'parts/sidebar/digital-leap' );  	echo '- 46 -';
+		get_template_part( 'parts/sidebar/digital-leap' );  	
 	}
 	?>
 
 	<?php 
 	if( is_post_type_archive( array('webinars', 'special-reports', 'ercs')) || is_search() || is_singular('whitepapers') || is_tax('sponsor')) { 
-		get_template_part( 'parts/sidebar/resources' );echo '- 52 -';
+		get_template_part( 'parts/sidebar/resources' );
 	} ?>
 
 	<?php
 	if( is_singular('webinars')) { 
-		get_template_part( 'parts/sidebar/sponsored-by' );  echo '- 57 -';
+		get_template_part( 'parts/sidebar/sponsored-by' );  
 	}
 	?>
 
 
 	<?php if(is_singular( array('webinars', 'events') )) {
-		get_template_part( 'parts/sidebar/speakers' );echo '- 63 -';
+		get_template_part( 'parts/sidebar/speakers' );
 	}
 
 	?>
 
 	<?php if( is_tag() || is_page('Top Stories')) {
-		get_template_part( 'parts/sidebar/tag-cloud' );echo '- 69 -';
+		get_template_part( 'parts/sidebar/tag-cloud' );
 
 	} ?>
 
 	<?php if(is_front_page() && is_home() || is_category() || is_page('Top Stories')){
-		get_template_part( 'parts/sidebar/professional-development' );echo '- 74 -';
+		get_template_part( 'parts/sidebar/professional-development' );
 
 	} ?>
 
 	<?php
 	if( is_singular('post')) { 
-		get_template_part( 'parts/sidebar/related-content' );  echo '- 80 -';
+		get_template_part( 'parts/sidebar/related-content' );  
 
 	}
 	?>
 
 	<?php
 	if( is_post_type_archive('whitepapers' ) || is_tax('company_categories')) { 
-		get_template_part( 'parts/sidebar/company-categories' );  	echo '- 87 -';
+		get_template_part( 'parts/sidebar/company-categories' );  	
 	}
 	?>
 
 
 	<?php
 	if( is_category()) { 
-			get_template_part( 'parts/ads/embeddedbanner' );echo '- 94 -';
 		
-			echo '<!-- this is a cat -->';
-			
-			get_template_part( 'parts/ads/embeddedbanner-2' );
 	}
 	?>
-
+<?php
+if(is_archive())
+    echo ' 99 ';
+?>
 	<?php 
 
 	if ( is_singular('whitepapers') || in_category('leading-the-digital-leap')) {
 
-		echo '';echo '- 106 -';
+		echo '';
 
 	} else {
 
-		get_template_part( 'parts/ads/embeddedbanner-2' ); echo '- 110 -';
+		get_template_part( 'parts/ads/embeddedbanner-2' ); 
 
 	} ?>
 
 	<?php if ( is_home() || is_front_page() ) {
-		get_template_part( 'parts/sidebar/links' );echo '- 115 -';
+		get_template_part( 'parts/sidebar/links' );
 
 	} ?>
 	
