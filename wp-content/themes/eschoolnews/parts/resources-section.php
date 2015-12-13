d0b0f66ddaf3f0742a82415b22e2716b3cc3e201
Erika Lewis <me.erikawithak@gmail.com>
2015-12-13 17:18:12 -0500

    updated resources section to include only active ercs

--- wp-content/themes/eschoolnews/parts/resources-section.php
+++ wp-content/themes/eschoolnews/parts/resources-section.php
@@ -9,35 +9,7 @@
 $resourcessection = 0;
 ?>
 
-<?php // The Query
-
-		if( is_home() || is_front_page()) {
-			$resources = new WP_Query(array(
-				'post_type' => array( 'ercs', 'specialreports'),
-				'posts_per_page' => '6'
-				)); 
-
-		} elseif ( is_category()) {
-			
-			get_template_part( 'parts/category-tags' );
-
-			$resources = new WP_Query(array(
-				'cat' => $cat,
-				'tag' => $categorytags,
-				'posts_per_page' => '6',
-				'post_type' => array( 'ercs', 'specialreports')
-			)); 
-
-					}?>
-
-
-				<?php if ( $resources->have_posts() ) : ?> 
-                <?php $resourcessection = 1; ?>
-
-<div class="row">
-	<div class="small-12 medium-12 columns">
-		
-		<?php if(is_category()) {
+<?php if(is_category()) {
 		echo '<h4>';
 		single_cat_title();
 		echo ' Resources</h4>';
@@ -46,57 +18,64 @@ $resourcessection = 0;
 		echo '<h1 class="section-title"><span><div class="icon resources"></div> Resources</span></h1>';
 	} ?>
 
+<?php // The Query
+
+//if( is_home() || is_front_page()) {
+
+	//first query
+	$ercs = get_posts(array(
+			'post_type' => 'ercs',
+			'status' => 'active-erc'
+	 		));
+	//second query
+	$specialreports = get_posts(array(
+			'post_type' => 'special-reports'
+	 		));
+	$resources = array_merge( $ercs, $specialreports ); //combine queries
+
+	foreach ( $resources as $post ) : setup_postdata( $post ); ?>
+
+	<?php $resourcessection = 1; ?>
 		
 
-				<?php while ( $resources->have_posts() ) : $resources -> the_post(); ?>
-				
-					
-						<article class="row">
+	<article class="row">
+		<div class="small-12 medium-4 columns">
 
-						
-					<div class="small-12 medium-4 columns">
+			<?php if( has_post_thumbnail()){
+				the_post_thumbnail('small');
+			} ?>
 
-						<?php if( has_post_thumbnail()){
-							the_post_thumbnail('small');
-						} ?>
-
-					</div>
-
-						<header class="small-12 medium-8 columns"> 
-							<?php get_template_part('parts/flags'); ?>
-				<h4> <a href="<?php the_permalink();?>"><?php the_title( ); ?></a></h4>
-								
-							<?php if( 'webinars' == get_post_type()){ ?>
-							<h5><i class="fi-calendar"></i> <?php 
-							$showdate = DateTime::createFromFormat('Ymd', get_field('event_date'));
-							if($showdate){ echo $showdate -> format('F d, Y');} ?></h5>
-							<h5><i class="fi-clock"></i> <?php the_field('event_time'); ?></h5>
-
-							<?php } ?>
-
-							<?php the_field('masthead_text') ?>
-
-
-						</header>
-
-					</article>
-
-					<hr>
-				
-
-					<?php endwhile; ?>
-
-					<?php wp_reset_query(); ?>
-
-				
-
-			
-
-
-				<h6 class="readmore"><a href="<?php echo home_url(); ?>/resources">See All Resources &raquo;</a></h6>
-
-			</div>
 		</div>
 
-		<?php endif;?>
+		<header class="small-12 medium-8 columns"> 
+			<?php get_template_part('parts/flags'); ?>
+			<h4> <a href="<?php the_permalink();?>"><?php the_title( ); ?></a></h4>
+
+			<?php if( 'webinars' == get_post_type()){ ?>
+			<h5><i class="fi-calendar"></i> <?php 
+			$showdate = DateTime::createFromFormat('Ymd', get_field('event_date'));
+			if($showdate){ echo $showdate -> format('F d, Y');} ?></h5>
+			<h5><i class="fi-clock"></i> <?php the_field('event_time'); ?></h5>
+
+			<?php } ?>
+
+			<?php the_field('masthead_text') ?>
+
+
+		</header>
+
+	</article>
+
+	<hr>
+
+
+
+	<?php endforeach; 
+	wp_reset_postdata();?>
+
+
+
+
+	<h6 class="readmore"><a href="<?php echo home_url(); ?>/resources">See All Resources &raquo;</a></h6>
+
 
