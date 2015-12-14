d512a64091a25a39b3160609af3fd93f9a50ed65
Erika Lewis <me.erikawithak@gmail.com>
2015-12-14 10:56:06 -0500

    events

--- wp-content/themes/eschoolnews/archive-events.php
+++ wp-content/themes/eschoolnews/archive-events.php
@@ -107,9 +107,6 @@ if (false === ($local_box_cache) ){
 				<article class="row">
 			<header class="small-12 columns">
 				<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				<div class="small-caps">By <?php the_author(); ?></div>
-				<div class="posted-on">Posted on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?></div>		
-
 			</header>
 		</article>
 
