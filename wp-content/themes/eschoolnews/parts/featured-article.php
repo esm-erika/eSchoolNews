8f8458bad26c630bd9c44f70cd150b5d4246d12c
vincentcarlson <vcarlson@eschoolmedia.com>
2015-12-23 11:50:37 -0500

    featured story fix

--- wp-content/themes/eschoolnews/parts/featured-article.php
+++ wp-content/themes/eschoolnews/parts/featured-article.php
@@ -99,7 +99,8 @@
 					<span class="flag"><a href="<?php the_permalink(); ?>">Featured</a></span>
 				<h2>
 					<a href="<?php the_permalink(); ?>">
-					<?php the_title(); echo '<!-- ' .get_the_ID(). ' -->';?>
+					<?php the_title(); echo '<!-- ' .get_the_ID(). ' posts-id= ' . $posts-ID . ' -->';?>
+                    
 				</a>
 			</h2>
 <pre>
