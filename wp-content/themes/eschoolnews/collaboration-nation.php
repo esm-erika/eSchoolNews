def756a716ceac1e9dea9d7fb624ce56c9f13e26
Erika Lewis <me.erikawithak@gmail.com>
2016-01-04 16:38:46 -0500

    uncomment out jquery

--- wp-content/themes/eschoolnews/collaboration-nation.php
+++ wp-content/themes/eschoolnews/collaboration-nation.php
@@ -158,8 +158,8 @@
 	<?php wp_footer(); ?>
 	<?php do_action( 'foundationpress_before_closing_body' ); ?>
 
-<!-- <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script> -->
-<!-- <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/slick/slick.min.js"></script>
 
 <script>
