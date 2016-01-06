f5be3bc9c833bcd6153e33c82223d1dd79663988
Erika Lewis <me.erikawithak@gmail.com>
2016-01-06 14:54:50 -0500

    ERC social studies

---
+++ wp-content/themes/eschoolnews/single-ercs-discovery-education-social-studies.php
@@ -0,0 +1,195 @@
+<?php /* Template Name: Discovery ERC Home Template */ ?>
+
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?> >
+<head>
+	<!-- /* Fonts */ -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,600,700|Merriweather:300|Cinzel+Decorative|Forum' rel='stylesheet' type='text/css'>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png">
+
+	<!-- /* Foundation Icons */ -->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/foundation-icons/foundation-icons.css">
+
+	<!-- /* Slick Carousel */ -->
+	<!-- <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/slick/slick.css"/> -->
+
+	<!-- slick-theme.css default styling -->
+	<!-- <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/slick/slick-theme.css"/> -->
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> id="discovery-erc">
+
+	<?php do_action( 'foundationpress_after_body' ); ?>
+
+	<div class="off-canvas-wrap" data-offcanvas>
+		<div class="inner-wrap">
+
+			<?php do_action( 'foundationpress_layout_start' ); ?>
+
+			<!-- Start Content -->
+		<section class="social-studies">
+			
+			<header>
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/banner-socialstudies_discovery-erc.jpg" alt="Powerful Practices">
+				<nav class="social-studies">
+
+					<div class="row">
+						<div class="small-12 columns">
+							<span class="studies-menu"><strong>> RESOURCES:</strong> <a href="<?php home_url(); ?>/ercs/discovery-education-social-studies">SOCIAL STUDIES</a></span>
+							<span class="math-menu"><a href="<?php home_url(); ?>/ercs/discovery-education-math">MATH</a></span>
+							<span class="science-menu"><a href="<?php home_url(); ?>/ercs/discovery-education-science">SCIENCE</a></span>
+							<span class="event-menu"><strong>> EVENTS:</strong> <a href="#">Powerful Practices: The Instructional Leadership Experience</a></span>
+						</div>
+					</div>
+
+				</nav>
+			</header>
+
+			<div class="row">
+				<div class="small-12 medium-8 columns">
+					<h3>Discovery Education Social Studies Techbook&trade;</h3>
+
+					<p>Social Studies Techbook is a core digital textbook that makes teaching and learning an unforgettable experience. Using an inquiry-based approach that enhances literacy and critical thinking skills, Social Studies Techbook supports any device, anywhere, anytime in any instructional setting.</p>
+
+					<p>
+						<a href="#">
+							<img width="120" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button_learn-more.png" alt="Learn More">
+						</a>
+					</p>
+
+					<h5 style="color: #666;">What Educators Can Do with Social Studies Techbook</h5>
+					<hr>
+
+					<h4>Maximize their time to plan, prepare, and deliver high quality instruction</h4>
+					<p>Teachers can access model lessons, primary and secondary source documents, interactive tools and activities, document-based investigations, and assessments all in one place at their fingertips.</p>
+
+
+					<h4>Engage students with multimedia content that is current and relevant</h4>
+					<p>The combination of videos, photographs, audio recordings, interactive maps, and variety of activities makes Social Studies more interesting and exciting to students. Social Studies Techbook is updated regularly and the Global Wrap feature includes weekly current events videos provided by MacNeil/Lehrer productions.</p>
+
+					<h4>Strengthens literacy, critical thinking, and citizenship skills for all students</h4>
+					<p>The inquiry-based format and interactive investigations emphasize informational text literacy, analytical writing, and problem solving skills that students will apply in academics and as citizens.</p>
+
+					<h4>Differentiate instruction for 21st century learners</h4>
+					<p>Students can use digital resources to help them process information from multiple sources. A variety of reading tools, including two different text levels, text-to-speech, and Spanish translations help students make meaning from the text. Formative and summative assessments are already built and ready for teachers and students to use.</p>
+
+					<h6><a href="http://www.DiscoveryEducation.com/SocialStudies">Find out more or sign up for a free trial at:  DiscoveryEducation.com/SocialStudies.</a></h6>
+					<br>
+					<div class="video">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/o7U8Sdfzy7E" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+				<div id="sidebar" class="small-12 medium-4 columns">
+					<h3>Resources</h3>
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Whitepaper</h6>
+								<h5>Five Strategies for Using Primary Source Documents</h5>	
+								
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/pdf/five-strategies-for-using-primary-source-documents-in-the-social-studies-classroom.pdf">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/five-strategies_discovery-erc.png" alt="">
+
+							</div>
+						</div>
+					</article>
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Ed-Tech Point of View</h6>
+								<h5>Building Content Knowledge and Inquiry Skills</h5>	
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/pdf/Building-Content-Knowledge-and-Inquiry-Skills-with-Discovery-Education-Social-Studies-Techbook.pdf">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/building-content_discovery-erc.png" alt="">
+
+							</div>
+						</div>
+					</article>
+
+					<img class="ad" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/300x250_discovery-erc.png" alt="">
+
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Whitepaper</h6>
+								<h5>From the Heart: Chris Layton</h5>	
+								
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/pdf/From-the-Heart_Chris-Layton.pdf">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/chris-layton_discovery-erc.png" alt="">
+
+							</div>
+						</div>
+					</article>
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Whitepaper</h6>
+								<h5>From the Heart: Queenie Hall</h5>	
+								
+								<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/pdf/FTH-Queenie.pdf">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/queenie-hall_discovery-erc.png" alt="">
+
+							</div>
+						</div>
+					</article>
+				</div>
+
+			</div>
+			<br>
+
+			<div class="row">
+				<div class="small-12 columns text-center">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/leaderboard_discovery-erc.png" alt="">
+				</div>
+			</div>
+
+			<br>
+
+
+
+
+
+</section>
+
+		<!-- End Content -->
+
+		</div>
+		<a class="exit-off-canvas"></a>
+
+	</div>
+	<?php do_action( 'foundationpress_layout_end' ); ?>
+
+	<?php wp_footer(); ?>
+	<?php do_action( 'foundationpress_before_closing_body' ); ?>
+
+</body>
+</html>
