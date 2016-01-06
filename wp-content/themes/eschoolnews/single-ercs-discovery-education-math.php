987fc5f496622f7fa4ff46bfa503b1d16e424794
Erika Lewis <me.erikawithak@gmail.com>
2016-01-06 10:40:17 -0500

    add math ERC

---
+++ wp-content/themes/eschoolnews/single-ercs-discovery-education-math.php
@@ -0,0 +1,205 @@
+<?php /* Template Name: Discovery ERC Home Template */ ?>
+
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?> >
+<head>
+	<!-- /* Fonts */ -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,600,700|Merriweather:300|Cinzel+Decorative|Forum' rel='stylesheet' type='text/css'>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png">
+
+	<!-- /* Foundation Icons */ -->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/foundation-icons/foundation-icons.css">
+
+	<!-- /* Slick Carousel */ -->
+	<!-- <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/slick/slick.css"/> -->
+
+	<!-- slick-theme.css default styling -->
+	<!-- <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/slick/slick-theme.css"/> -->
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> id="discovery-erc">
+
+	<?php do_action( 'foundationpress_after_body' ); ?>
+
+	<div class="off-canvas-wrap" data-offcanvas>
+		<div class="inner-wrap">
+
+			<?php do_action( 'foundationpress_layout_start' ); ?>
+
+			<!-- Start Content -->
+		<section class="math">
+			
+			<header>
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/banner-math_discovery-erc.png" alt="Powerful Practices">
+				<nav class="math">
+
+					<div class="row">
+						<div class="small-12 columns">
+							<span class="math-menu"><strong>> RESOURCES:</strong> <a href="#">MATH</a></span>
+							<span class="science-menu"><a href="#">SCIENCE</a></span>
+							<span class="studies-menu"><a href="#">SOCIAL STUDIES</a></span>
+							<span class="event-menu"><strong>> EVENTS:</strong> <a href="#">Powerful Practices: The Instructional Leadership Experience</a></span>
+						</div>
+					</div>
+
+				</nav>
+			</header>
+
+			<div class="row">
+				<div class="small-12 medium-8 columns">
+					<h3>Learning Math Starts with a Connection.</h3>
+
+					<p>Math Techbook is a breakthrough digital textbook that will change the way students and
+					teachers experience math. Using an inquiry-based approach, Math Techbook engages
+					students with real world problems worth solving while supporting any device, anywhere,
+					anytime in any secondary instructional setting.</p>
+
+					<p>
+						<a href="#">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button_learn-more.png" alt="Learn More">
+						</a>
+					</p>
+
+					<h5 style="color: #666;">What Educators Can Do with Math Techbook</h5>
+					<hr>
+
+					<h4>Teach using an inquiry-based approach</h4>
+					<p>Math Techbook uses a Discover, Practice, Apply cycle that balances conceptual learning,
+					procedural fluency, and application to real world problems so students gain lasting
+					proficiency.</p>
+
+
+					<h4>Engage Students First to Make Learning Last</h4>
+					<p>Students can access dynamic content, interactives, videos, digital tools, and game-like
+					activities that increase their motivation to learn math. Students learn content through
+					multiple pathways that match their learning style and can monitor their own progress in
+					real time with the Student Dashboard.</p>
+
+					<h4>Differentiate Instruction for 21st Century Learners</h4>
+					<p>The newest generation of technology enhanced items and formative assessments are
+					woven throughout the entire instructional cycle. Teachers can collect evidence of
+					student progress instantly.</p>
+
+					<h4>Save Teachers Time in Planning and Preparation</h4>
+					<p>Comprehensive resources, student activities, and model lessons are at teachers'
+					fingertips in one, easy-to-navigate site. Real-time feedback and easy-to-read data on
+					the Teacher Dashboard make it easier to adjust instruction.</p>
+
+					<div class="video">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/hF5xONSlQ9Q" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+				<div id="sidebar" class="small-12 medium-4 columns">
+					<h3>Resources</h3>
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Infographic</h6>
+								<h5>Problems Worth Solving</h5>
+								<a href="#"></a>
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+								
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/infographic_discovery-erc.png" alt="Infographic">
+
+							</div>
+						</div>
+					</article>
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Ed-Tech Point of View</h6>
+								<h5>Transforming Math for a Digital Era</h5>	
+								<a href="#">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/math-cover_discovery-erc.png" alt="Infographic">
+
+							</div>
+						</div>
+					</article>
+
+					<img class="ad" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/300x250_discovery-erc.png" alt="">
+
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Whitepaper</h6>
+								<h5>From the Heart: Stories from Passionate Educators</h5>	
+								
+								<a href="#">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-download_discovery-erc.png" alt="Download">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/from-the-heart_discovery-erc.png" alt="Infographic">
+
+							</div>
+						</div>
+					</article>
+
+					<article>
+						<div class="row">
+							<div class="small-12 medium-6 columns">
+								<h6 style="color: #666;">Video</h6>
+								<h5>Problems Worth Solving</h5>	
+								
+								<a href="#">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/button-watch_discovery-erc.png" alt="Watch">
+								</a>
+							</div>
+							<div class="small-12 medium-6 columns">
+								<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/bridge_discovery-erc.png" alt="Infographic">
+
+							</div>
+						</div>
+					</article>
+				</div>
+
+			</div>
+			<br>
+
+			<div class="row">
+				<div class="small-12 columns text-center">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/leaderboard_discovery-erc.png" alt="">
+				</div>
+			</div>
+
+			<br>
+
+
+
+
+
+</section>
+
+		<!-- End Content -->
+
+		</div>
+		<a class="exit-off-canvas"></a>
+
+	</div>
+	<?php do_action( 'foundationpress_layout_end' ); ?>
+
+	<?php wp_footer(); ?>
+	<?php do_action( 'foundationpress_before_closing_body' ); ?>
+
+</body>
+</html>
