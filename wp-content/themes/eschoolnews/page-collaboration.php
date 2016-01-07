34a64a1d64ec035e3d9c1c99b6440b35790260f0
Erika Lewis <me.erikawithak@gmail.com>
2016-01-07 17:21:43 -0500

    update countdown

--- wp-content/themes/eschoolnews/page-collaboration.php
+++ wp-content/themes/eschoolnews/page-collaboration.php
@@ -153,7 +153,18 @@ global $cat;
 				<h2 class="page-title"><?php the_title(); ?></h2>
 
 				<div class="calendar">
-					<span>168<br/>Days</span>
+					<span class="days"><?php
+
+						$date1 = new DateTime(date('Y-m-d'));  //current date or any date
+						$date2 = new DateTime("2016-05-15");   //Future date
+						$diff = $date2->diff($date1)->format("%a");  //find difference
+						$days = intval($diff);   //rounding days
+						echo $days;
+
+					?><br>
+					Days
+				</span>
+
 				</div>
 
 				<?php the_content(); ?>
