019bfa003ea005fe920ece0eaf706ad824abb085
vincentcarlson <vcarlson@eschoolmedia.com>
2016-01-14 12:12:09 -0500

    Partner RSS Feeds Cat Change

--- wp-content/themes/eschoolnews/feed-esnsmartbrief.php
+++ wp-content/themes/eschoolnews/feed-esnsmartbrief.php
@@ -111,7 +111,7 @@ echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>
 		
 		<!-- Start loop -->
         <?php /* while( have_posts()) : the_post(); */ ?>
-		 <?php $query = new WP_Query( 'cat=133' ); ?>
+		 <?php $query = new WP_Query( 'cat=11' ); ?>
  <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 
 			<item>
