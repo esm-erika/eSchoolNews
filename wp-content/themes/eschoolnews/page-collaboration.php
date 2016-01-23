2a59bd4c994fc51d62896b1624f5c340a067b06f
Erika Lewis <me.erikawithak@gmail.com>
2016-01-22 19:07:32 -0500

    update file

--- wp-content/themes/eschoolnews/page-collaboration.php
+++ wp-content/themes/eschoolnews/page-collaboration.php
@@ -194,7 +194,7 @@ global $cat;
 	<br><br>
 
 	<div class="row">
-		<div class="small-12 medium-7 columns submission">
+		<div class="small-12 medium-10 columns submission">
 			<h4>Submission Deadlines</h4>
 			<hr>
 			<ul>
@@ -219,11 +219,6 @@ Entries accepted from 4/1-4/30 and voting takes place from 5/1-5/15</li>
 		</div>
 
 		<div class="small-12 medium-4 columns twitter">
-			
-			<a href="http://www.eschoolnews.com/webinars/transforming-learning-through-collaboration/" target="_blank">
-				<img src="http://eschoolnews.esminc.staging.wpengine.com/files/2016/01/CNWebinar.jpg" alt="WEBINAR: 2015 Collaboration Nation Contest Winner">
-			</a>
-			<br><br>
 			<h4>Twitter <a href="#" class="twitter"><i class="fi-social-twitter"></i></a></h4>
 			<hr>
 			<p align="center">
