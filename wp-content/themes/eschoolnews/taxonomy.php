9f3161ad14feb1cdc763658241faf2f7a8dd86cc
Erika Lewis <me.erikawithak@gmail.com>
2016-01-25 11:55:41 -0500

    update sponsor pages

--- wp-content/themes/eschoolnews/taxonomy.php
+++ wp-content/themes/eschoolnews/taxonomy.php
@@ -104,12 +104,38 @@ if ( have_posts() ) {
 					<?php endwhile;	
 	
 	
-} // end if
-?>
+					} // end if
+					?>
 
 		
 
 		<?php  } elseif( is_tax('sponsor') ) { ?>
+
+		<?php 
+		
+		// the query
+			$the_query = new WP_Query(array(
+				//'post_type' => 'ercs',
+				'meta_query' => array(
+					array(
+						'key' => 'erc_status',
+						'value' => '1',
+						'compare' => '=='
+						)
+					),
+				'posts_per_page' => -1
+				));	
+
+
+		
+		 ?>
+
+		<?php if ( $the_query->have_posts() ) : ?>
+
+			<!-- pagination here -->
+
+			<!-- the loop -->
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
 				<article class="row">
 						<div class="small-12 medium-4 columns">
@@ -135,6 +161,17 @@ if ( have_posts() ) {
 				</article>
 
 				<hr/>
+
+				<?php endwhile; ?>
+			<!-- end of the loop -->
+
+			<!-- pagination here -->
+
+			<?php wp_reset_postdata(); ?>
+
+		<?php else : ?>
+			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+		<?php endif; ?>	
 
 	
 
