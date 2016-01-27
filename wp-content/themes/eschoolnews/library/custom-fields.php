124a6da45786f5cfac9884456202e70fbbd4403a
Erika Lewis <me.erikawithak@gmail.com>
2016-01-27 11:54:10 -0500

    update sponsor placement for ercs

--- wp-content/themes/eschoolnews/library/custom-fields.php
+++ wp-content/themes/eschoolnews/library/custom-fields.php
@@ -471,6 +471,45 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 
+	register_field_group(array (
+		'id' => 'acf_erc-sponsor-placement',
+		'title' => 'ERC Sponsor Placement',
+		'fields' => array (
+			array (
+				'key' => 'field_56a8eb9b7a084',
+				'label' => 'Sponsor Placement',
+				'name' => 'sponsor_placement',
+				'type' => 'radio',
+				'choices' => array (
+					'left' => 'Left',
+					'right' => 'Right',
+				),
+				'other_choice' => 0,
+				'save_other_choice' => 0,
+				'default_value' => '',
+				'layout' => 'vertical',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'ercs',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'side',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+
 
 
 /*===================================================================================
--- wp-content/themes/eschoolnews/single-ercs.php
+++ wp-content/themes/eschoolnews/single-ercs.php
@@ -401,8 +401,34 @@ if (false === ($local_box_cache) ){
 
 			?>
 
-			
+			<?php 
 
+			if (get_field('sponsor_placement') == 'left') { ?>
+
+				<?php 
+
+				$taxonomy = 'sponsor';
+				$terms = get_the_terms( $post->ID, $taxonomy);
+				$term_id = $terms[0]->term_id;
+
+				$image = get_field('sponsor_image', $taxonomy . '_' . $term_id);
+				
+				if( !empty($image) ): ?>
+
+					
+						<div class="sponsored">
+
+							<small>Sponsored By:</small><br>
+
+							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+						</div>
+
+					
+
+				<?php endif; ?>
+			
+			<?php } ?>
 
 	 	</div>
 		
@@ -655,7 +681,34 @@ if (false === ($local_box_cache) ){
 
 			?>
 
-			<?php get_template_part('parts/sponsor-images'); ?>
+			<?php 
+
+			if (get_field('sponsor_placement') == 'right') { ?>
+
+					<?php 
+
+					$taxonomy = 'sponsor';
+					$terms = get_the_terms( $post->ID, $taxonomy);
+					$term_id = $terms[0]->term_id;
+
+					$image = get_field('sponsor_image', $taxonomy . '_' . $term_id);
+					
+					if( !empty($image) ): ?>
+
+						
+							<div class="sponsored right">
+
+								<small>Sponsored By:</small><br>
+
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+
+							</div>
+
+						
+
+					<?php endif; ?>
+
+			<?php } ?>
 
 	 	</div>
 	 </div>
