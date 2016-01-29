fd12149e97259a497d27f28b1d4bb3f38c9c8e7b
Erika Lewis <me.erikawithak@gmail.com>
2016-01-29 17:08:51 -0500

    udpate landing pages

--- wp-content/themes/eschoolnews/parts/landing-pages/technology.php
+++ wp-content/themes/eschoolnews/parts/landing-pages/technology.php
@@ -1,7 +1,7 @@
 <?php LandingRecentItems('it-management', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-1' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-1' ); ?>
 <?php LandingRecentItems('networking-and-broadband', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-2' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-2' ); ?>
 <?php LandingRecentItems('professional-development', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-3' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-3' ); ?>
 <?php LandingRecentItems('safety-and-security', 3) ?>
--- wp-content/themes/eschoolnews/parts/landing-pages/thought-leadership.php
+++ wp-content/themes/eschoolnews/parts/landing-pages/thought-leadership.php
@@ -1,11 +1,11 @@
 <?php LandingRecentItems('future-ready-schools', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-1' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-1' ); ?>
 <?php LandingRecentItems('policy', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-2' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-2' ); ?>
 <?php LandingRecentItems('professional-development', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-3' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-3' ); ?>
 <?php LandingRecentItems('research', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-4' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-4' ); ?>
 <?php LandingRecentItems('school-administration', 3) ?>
-<?php get_template_part( 'parts/ads/landingpage-5' ); ?>
+<?php get_template_part( 'parts/ads/lb-landingpage-5' ); ?>
 <?php LandingRecentItems('superintendents-center', 3) ?>
