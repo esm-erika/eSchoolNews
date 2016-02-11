f44f9567fa69121667e95bdeb8e0fd8d9ea50c34
Erika Lewis <me.erikawithak@gmail.com>
2016-02-11 14:58:09 -0500

    add archive page for symposiusm

---
+++ wp-content/themes/ecampusnews/archive-symposiums.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+<?php 
+//insert cache query
+global $page;
+$box_qt = 'esm_c_arcSR_menu_pg'.$page;
+$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
+$local_box_cache = get_transient( $box_q );
+if (false === ($local_box_cache) ){
+	// start code to cache
+		ob_start( );
+		echo '<!-- c -->'; 
+		?>
+
+<div class="row">
+
+		<?php get_template_part( 'parts/section-titles' ); ?>
+
+
+<!-- Row for main content area -->
+	<div class="small-12 medium-8 columns" role="main">
+
+		   
+
+
+				<?php 
+
+				$taxonomy = 'subjects';
+				$terms = get_the_terms( $post->ID, $taxonomy);
+				$term_id = $terms[0]->term_id;
+
+				$image = get_field('subjects_image', $taxonomy . '_' . $term_id);
+				//$url = get_field('sponsor_url', $taxonomy . '_' . $term_id);
+				
+				if( !empty($image) ): ?>
+
+
+				<div class="row">
+					<div class="small-12 columns">
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					</div>
+				</div>
+
+				<?php endif; ?>
+
+
+				 <?php
+
+				// The Query
+				$args = array(
+					'post_type' => 'symposiums',
+					'posts_per_page' => '2',
+					'orderby' => 'date'
+					);
+
+				$query = new WP_Query( $args ); ?>
+
+				<article class="row">
+
+
+
+				<?php // The Loop
+				 while ( $query->have_posts() ) :
+					$query->the_post(); ?>
+
+				
+							<div class="small-12 medium-6 columns">
+
+								<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+
+								<?php the_excerpt(); ?>
+
+								
+							</div>
+						
+					
+					<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+
+									</article>
+
+
+		</div>
+
+
+		
+		<?php
+		echo '<!-- c '.date(DATE_RFC2822).' -->' ;
+		$local_box_cache = ob_get_clean( );
+	// end the code to cache
+		echo $local_box_cache;
+	//end cache query 
+	
+	if( current_user_can( 'edit_post' ) ) {
+		//you cannot cache it
+	} else {
+		set_transient($box_q ,$local_box_cache, 60 * 10);
+	}
+} else { 
+
+echo $local_box_cache;
+
+}
+?>        
+        
+<?php 
+//insert cache query
+//name format esm_c_[template name in 5 char]_a[ast]c[astc]c[category]p[post id(if sidebar needs to be unique]t[if a tag page]
+//$cat_name = get_category(get_query_var('cat'))->term_id;
+
+global $astc, $astused;
+$box_qt = 'esm_c_sra_a'.$astused."c".$astc.'c'.'p';
+$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
+	
+$local_box_cache = get_transient( $box_q );
+if (false === ($local_box_cache) ){
+
+	// start code to cache
+		ob_start( );
+			echo '<!-- c -->';
+			get_sidebar();
+			echo '<!-- c '.date(DATE_RFC2822).' -->' ;
+		$local_box_cache = ob_get_clean( );
+	// end the code to cache
+		echo $local_box_cache;
+	//end cache query 
+	
+	if( current_user_can( 'edit_post' ) ) {
+		//you cannot cache it
+	} else {
+		set_transient($box_q ,$local_box_cache, 60 * 10);
+	}
+} else { 
+
+echo $local_box_cache;
+
+}
+?>
+	
+
+	
+
+			<hr class="thick"/>
+
+			<h4>View Our Past Symposiums</h4>
+
+			<br/>
+
+
+		
+	</div>
+	
+</div>
+<?php get_footer(); ?>
