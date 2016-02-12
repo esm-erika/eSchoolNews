f1aa162373994e486e69217d85381be9ad0cdd86
vincentcarlson <vcarlson@eschoolmedia.com>
2016-02-12 16:10:59 -0500

    landing pages update quanity on CC

--- wp-content/themes/ecampusnews/parts/landing-pages/community-colleges.php
+++ wp-content/themes/ecampusnews/parts/landing-pages/community-colleges.php
@@ -1,3 +1,3 @@
-<?php LandingRecentItems('community-college-blog', 3) ?>
+<?php LandingRecentItems('community-college-blog', 6) ?>
 <?php get_template_part( 'parts/ads/lb-landingpage-1' ); ?>
-<?php LandingRecentItems('community-college-2', 3) ?>
+<?php LandingRecentItems('community-college-2', 6) ?>
