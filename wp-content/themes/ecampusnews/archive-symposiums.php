a41e2860bfde5afadb4334b9d840e15fdc655382
Erika Lewis <me.erikawithak@gmail.com>
2016-02-15 16:18:47 -0500

    update symposiums

--- wp-content/themes/ecampusnews/archive-symposiums.php
+++ wp-content/themes/ecampusnews/archive-symposiums.php
@@ -38,7 +38,27 @@ if (false === ($local_box_cache) ){
 	<div class="small-12 medium-12 columns" role="main">
 
 
-		Test
+		<?php 
+		// the query
+		$the_query = new WP_Query( $args ); ?>
+
+		<?php if ( $the_query->have_posts() ) : ?>
+
+			<!-- pagination here -->
+
+			<!-- the loop -->
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+			<?php endwhile; ?>
+			<!-- end of the loop -->
+
+			<!-- pagination here -->
+
+			<?php wp_reset_postdata(); ?>
+
+		<?php else : ?>
+			<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+		<?php endif; ?>
 		
 		</div>
 
