b1b5fcbdecf250223fd820aef391445e65b2134b
vincentcarlson <vcarlson@eschoolmedia.com>
2016-02-16 13:37:20 -0500

    comment change

--- wp-content/themes/ecampusnews/comments.php
+++ wp-content/themes/ecampusnews/comments.php
@@ -65,20 +65,20 @@ endif;
 	}
 ?>
 
-<?php
+<?php echo '68';
 if ( comments_open() ) :
-	if ( (is_page() || is_single() || is_singular()) && ( ! is_home() && ! is_front_page()) ) :
-?>
+echo '70';	if ( (is_page() || is_single() || is_singular()) && ( ! is_home() && ! is_front_page()) ) :
+echo '71';?>
 <section id="respond">
 	<h3><?php comment_form_title( __( 'Leave a Reply', 'foundationpress' ), __( 'Leave a Reply to %s', 'foundationpress' ) ); ?></h3>
 	<p class="cancel-comment-reply"><?php cancel_comment_reply_link(); ?></p>
-	<?php if ( get_option( 'comment_registration' ) && ! is_user_logged_in() ) : ?>
+	<?php if ( get_option( 'comment_registration' ) && ! is_user_logged_in() ) :  echo '75';?>
 	<p><?php printf( __( 'You must be <a href="%s">logged in</a> to post a comment.', 'foundationpress' ), wp_login_url( get_permalink() ) ); ?></p>
-	<?php else : ?>
+	<?php else : echo '77';?>
 	<form action="<?php echo get_option( 'siteurl' ); ?>/wp-comments-post.php" method="post" id="commentform">
 		<?php if ( is_user_logged_in() ) : ?>
 		<p><?php printf( __( 'Logged in as <a href="%s/wp-admin/profile.php">%s</a>.', 'foundationpress' ), get_option( 'siteurl' ), $user_identity ); ?> <a href="<?php echo wp_logout_url( get_permalink() ); ?>" title="<?php __( 'Log out of this account', 'foundationpress' ); ?>"><?php _e( 'Log out &raquo;', 'foundationpress' ); ?></a></p>
-		<?php else : ?>
+		<?php else : echo '81';?>
 		<p>
 			<label for="author">
 				<?php
@@ -103,7 +103,7 @@ if ( comments_open() ) :
 			</label>
 			<input type="text" class="five" name="url" id="url" value="<?php echo esc_attr( $comment_author_url ); ?>" size="22" tabindex="3">
 		</p>
-		<?php endif; ?>
+		<?php endif; echo '106';?>
 		<p>
 			<label for="comment">
 					<?php
@@ -124,7 +124,7 @@ if ( comments_open() ) :
 		<?php comment_id_fields(); ?>
 		<?php do_action( 'comment_form', $post->ID ); ?>
 	</form>
-	<?php endif; // If registration required and not logged in. ?>
+	<?php endif; echo '127';// If registration required and not logged in. ?>
 </section>
 <?php
 	endif; // If you delete this the sky will fall on your head.
