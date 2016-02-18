a4e940044e135d55408d876f35efb05e2853f186
Erika Lewis <me.erikawithak@gmail.com>
2016-02-18 11:26:09 -0500

    update buttons

--- wp-content/themes/eschoolnews/single-whitepapers.php
+++ wp-content/themes/eschoolnews/single-whitepapers.php
@@ -109,7 +109,7 @@ get_header(); ?>
 								echo '<p>';
 								if ($WPcbt != null) { 
 
-									echo'<a href="'.$WPURL.'" target="_blank"><img class="alignright" src="'.$WPcbt.'" alt="Next" border="0" /></a>';
+									echo'<a href="'.$WPURL.'" target="_blank">Submit and Download</a>';
 								} else{
 									echo'<a class="button radius small" href="'.$WPURL.'">Download White Paper</a>';
 								}
