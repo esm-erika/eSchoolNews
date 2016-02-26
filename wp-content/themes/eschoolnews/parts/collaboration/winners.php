3dbe491d3e41cb0387f71c1983425a09e17cfcef
Erika Lewis <me.erikawithak@gmail.com>
2016-02-26 17:07:33 -0500

    update and test winners

--- wp-content/themes/eschoolnews/parts/collaboration/winners.php
+++ wp-content/themes/eschoolnews/parts/collaboration/winners.php
@@ -4,39 +4,63 @@
 
 		<section id="recent-entries">
 
-					<h2 class="text-center">2016 Entries</h2>
+			<h2 class="text-center">2016 Entries</h2>
 
-					<?php
+			<?php 
+// the query
+			$the_query = new WP_Query( $args ); ?>
+
+			<?php if ( $the_query->have_posts() ) : ?>
+
+			<!-- the loop -->
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+
+			<?php
 
 					// check if the repeater field has rows of data
-					if( have_rows('video_section') ):
+			if( have_rows('video_section') ):
 
 					 	// loop through the rows of data
-					    while ( have_rows('video_section') ) : the_row(); ?>
-
-						
-						<?php 
-
-							$image = get_sub_field('entry_video');
-
-							if( !empty($image) ): ?>
-
-								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-
-							<?php endif; ?>
+				while ( have_rows('video_section') ) : the_row(); ?>
 
 
+			
+			<h2><?php the_title(); ?></h2>
 
-					<?php
 
-						endwhile;
+			
+			<?php 
 
-						else :
+			$image = get_sub_field('entry_video');
 
-						endif;
+			if( !empty($image) ): ?>
 
-					?>
-				</section>
+			<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 
-	</div>
+		<?php endif; ?>
+
+
+
+		<?php
+
+		endwhile;
+
+		else :
+
+			endif;
+
+		?>
+		
+		<?php endwhile;
+		wp_reset_postdata(); ?>
+
+	<?php endif; ?>
+
+	
+
+	
+</section>
+
+</div>
 </div>
