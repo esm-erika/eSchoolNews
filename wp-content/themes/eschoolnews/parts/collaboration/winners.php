666593199f7db2d0c37d1baa81c294baf5b18647
Erika Lewis <me.erikawithak@gmail.com>
2016-02-26 17:13:03 -0500

    update and test winners

--- wp-content/themes/eschoolnews/parts/collaboration/winners.php
+++ wp-content/themes/eschoolnews/parts/collaboration/winners.php
@@ -10,7 +10,7 @@
 
 			$args = array(
 				'post_type' => 'collabnation',
-				
+
 				);
 			// the query
 			$the_query = new WP_Query( $args ); ?>
@@ -20,42 +20,7 @@
 			<!-- the loop -->
 			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-
-			<?php
-
-					// check if the repeater field has rows of data
-			if( have_rows('video_section') ):
-
-					 	// loop through the rows of data
-				while ( have_rows('video_section') ) : the_row(); ?>
-
-
-
 			<h2><?php the_title(); ?></h2>
-
-
-
-			<?php 
-
-			$image = get_sub_field('entry_video');
-
-			if( !empty($image) ): ?>
-
-			<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-
-		<?php endif; ?>
-
-
-
-		<?php
-
-		endwhile;
-
-		else :
-
-			endif;
-
-		?>
 
 		<?php endwhile;
 		wp_reset_postdata(); ?>
