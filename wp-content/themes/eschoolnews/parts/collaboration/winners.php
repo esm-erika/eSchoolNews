ad763cfda70ce5c54efe3249f0700d846a17d1c7
Erika Lewis <me.erikawithak@gmail.com>
2016-02-26 15:50:29 -0500

    winners page

--- wp-content/themes/eschoolnews/parts/collaboration/winners.php
+++ wp-content/themes/eschoolnews/parts/collaboration/winners.php
@@ -27,6 +27,59 @@
 				<div class="row">
 				<div class="small-12 medium-10 medium-centered columns">
 
+					<section id="recent-entries">
+
+					<?php 
+
+					$args2 = array(
+						'post_type' => 'collabnation',
+						'order' => 'ASC',
+						'tax_query' => array(
+							array(
+							'taxonomy' => 'years',
+							'field' => 'slug',
+							'terms' => '2016',
+
+								),
+
+							),
+
+					); 
+
+					$entries = new WP_Query($args2); ?>
+
+
+					<h3 class="text-center">2016 Entries</h3>
+
+					<div class="entries">
+
+					<?php if ( $entries->have_posts() ) : ?>
+
+					<?php while ( $entries->have_posts() ) : $entries->the_post(); ?>
+
+					  <div>
+
+					  	<?php if( get_field('entry_video')){ ?>
+
+					  	<div class="video">
+					  		<iframe width="420" height="315" src="https://www.youtube.com/embed/<?php the_field('entry_video') ?>" frameborder="0" allowfullscreen></iframe>
+					  	</div>
+
+					  	<h6 class="text-center"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+
+					  	<?php } ?>
+					  	
+					  </div>
+
+					<?php endwhile; 
+				wp_reset_postdata(); ?>
+
+				<?php endif; ?>
+
+
+					</div> 
+				</section>
+
 				<h4>Congratulations to Our 2015 Winners</h4>
 
 				<?php if ( $the_query->have_posts() ) : ?>
@@ -72,58 +125,7 @@
 				<?php endif; ?>
 
 
-				<section id="recent-entries">
-
-					<?php 
-
-					$args2 = array(
-						'post_type' => 'collabnation',
-						'order' => 'ASC',
-						'tax_query' => array(
-							array(
-							'taxonomy' => 'years',
-							'field' => 'slug',
-							'terms' => '2016',
-
-								),
-
-							),
-
-					); 
-
-					$entries = new WP_Query($args2); ?>
-
-
-					<h3 class="text-center">2016 Entries</h3>
-
-					<!-- <div class="entries">
-
-					<?php if ( $entries->have_posts() ) : ?>
-
-					<?php while ( $entries->have_posts() ) : $entries->the_post(); ?>
-
-					  <div>
-
-					  	<?php if( get_field('entry_video')){ ?>
-
-					  	<div class="video">
-					  		<iframe width="420" height="315" src="https://www.youtube.com/embed/<?php the_field('entry_video') ?>" frameborder="0" allowfullscreen></iframe>
-					  	</div>
-
-					  	<h6 class="text-center"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
-
-					  	<?php } ?>
-					  	
-					  </div>
-
-					<?php endwhile; 
-				wp_reset_postdata(); ?>
-
-				<?php endif; ?>
-
-
-					</div> -->
-				</section>
+				
 
 			</div>
 		</div>
