cfa83b37df4a09be3301a741d2e2986f5b3958e5
Erika Lewis <me.erikawithak@gmail.com>
2016-02-29 17:10:44 -0500

    update collaboration nation

--- wp-content/themes/eschoolnews/collaboration-nation.php
+++ wp-content/themes/eschoolnews/collaboration-nation.php
@@ -187,7 +187,9 @@
 	  $('.entries').slick({
 	    infinite: true,
 		  slidesToShow: 3,
-		  slidesToScroll: 3
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 2000,
 	  });
 	});
 </script>
