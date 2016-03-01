304536720539db9184de35a7368952f44a90e8e9
Erika Lewis <me.erikawithak@gmail.com>
2016-03-01 17:29:09 -0500

    newsletter updates

--- wp-content/themes/ecampusnews/single-newsletter.php
+++ wp-content/themes/ecampusnews/single-newsletter.php
@@ -1054,11 +1054,7 @@ table.columns .right-text-pad {
 
               <!-- End Masthead -->
 
-              <?php 
-
-              $intro = get_the_content();
-
-              if($intro != '') { ?>
+              <?php if(get_field('newsletter_introduction')) { ?>
 
               <table class="row" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; width: 100%; position: relative; display: block; padding: 0px;">
                 <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
@@ -1068,7 +1064,7 @@ table.columns .right-text-pad {
                       <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
                         <td class="text-pad" style="word-break: keep-all; -webkit-hyphens: none; -moz-hyphens: none; hyphens: none; border-collapse: collapse !important; vertical-align: top; text-align: left; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 0px 10px 10px;" align="left" valign="top">
 
-                          <?php the_content(); ?>
+                          <?php the_field('newsletter_introduction'); ?>
 
                         </td>
                       </tr>
