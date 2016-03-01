8f28dec35185a7999f9168a4be867693b9e5a74e
Erika Lewis <me.erikawithak@gmail.com>
2016-03-01 17:39:47 -0500

    update newsletter

--- wp-content/themes/ecampusnews/single-newsletter.php
+++ wp-content/themes/ecampusnews/single-newsletter.php
@@ -1383,7 +1383,7 @@ table.columns .right-text-pad {
 
                           <h6 style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 1.3; word-break: normal; font-size: 20px; margin: 0; padding: 0;" align="left">Connect With Us:</h6>
 
-                          <p style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 19px; font-size: 14px; margin: 0 0 10px; padding: 0;" align="left">eSchool News, 7920 Norfolk Ave Suite 900, Bethesda, MD 20814<br>
+                          <p style="color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; text-align: left; line-height: 19px; font-size: 14px; margin: 0 0 10px; padding: 0;" align="left">eCampus News, 7920 Norfolk Ave Suite 900, Bethesda, MD 20814<br>
                             Phone: 301-913-0115 <br>
                             <a href="http://www.ecampusnews.com" style="color: #2ba6cb; text-decoration: none;">www.ecampusnews.com</a> <br>
                             <a href="mailto:custserv@ecampusnews.com" style="color: #2ba6cb; text-decoration: none;">custserv@ecampusnews.com</a>
