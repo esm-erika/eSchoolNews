02f042a75b06eac3e763d91b051aad1b668b5359
Erika Lewis <me.erikawithak@gmail.com>
2016-03-03 11:42:54 -0500

    update newsletter template

--- wp-content/themes/ecampusnews/single-newsletter.php
+++ wp-content/themes/ecampusnews/single-newsletter.php
@@ -1350,6 +1350,8 @@ table.columns .right-text-pad {
 
               ?>
 
+              <?php if(get_field('current_issue')) { ?>
+
               <table class="row" style="border-spacing: 0; border-collapse: collapse; vertical-align: top; text-align: left; width: 100%; position: relative; display: block; padding: 0px;">
                 <tr style="vertical-align: top; text-align: left; padding: 0;" align="left">
                   <td class="wrapper last" style="word-break: keep-all; -webkit-hyphens: none; -moz-hyphens: none; hyphens: none; border-collapse: collapse !important; vertical-align: top; text-align: left; position: relative; color: #222222; font-family: 'Helvetica', 'Arial', sans-serif; font-weight: normal; line-height: 19px; font-size: 14px; margin: 0; padding: 10px 0px 0px;" align="left" valign="top">
@@ -1368,6 +1370,8 @@ table.columns .right-text-pad {
                   </td>
                 </tr>
               </table>
+
+              <?php } ?>
 
               <!--  </td></tr></table> -->
 
