4c64f4ae13489b267a4f626e232e8488046e74b8
Erika Lewis <me.erikawithak@gmail.com>
2016-03-14 15:23:13 -0400

    update about page

--- wp-content/themes/eschoolnews/page-about.php
+++ wp-content/themes/eschoolnews/page-about.php
@@ -21,45 +21,6 @@ get_header(); ?>
 	<?php do_action( 'foundationpress_before_content' ); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
-<?php  $astused = get_post_meta($id, '_wp_esmad_template', true);
-$oldtemplate = get_post_meta($id, '_wp_post_template', true);
-
-
-
-   ?>
-<?php if($oldtemplate){ echo '<!-- '.$oldtemplate.' -->'; //using old template
-	
-//require_once( 'library/boxes.php' );	
-include('single-coa.php');
-	
-	
-	
-	   } else { //not using old template ?>    
-    
-
-
-
-<?php 
-//insert cache query
-//name format esm_c_[template name in 5 char]_a[ast]c[astc][c ...category][p  ...post id(if sidebar needs to be unique][t ...(tagid)if a tag page][a ... Author ID (if an author page)]
-//$cat_name = get_category(get_query_var('cat'))->term_id;
-
-// $queried_object = get_queried_object();
-// var_dump( $queried_object );
-//$tag_id = get_query_var('term_taxonomy_id');
-$post_id = get_the_ID(); 
-//$cat_name = get_category(get_query_var('cat'))->term_id;
-global $astc, $astused;
-$box_qt = 'esm_c_pagebdy_a'.$ast."c".$astc.'p'.$post_id;
-$box_q = preg_replace("/[^A-Za-z0-9_ ]/", '', $box_qt);
-	
-$local_box_cache = get_transient( $box_q );
-if (false === ($local_box_cache) ){
-
-	// start code to cache
-		ob_start( );
-			echo '<!-- c -->';
-?>
 
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
@@ -77,29 +38,6 @@ if (false === ($local_box_cache) ){
 			<?php //comments_template(); ?>
 			<?php do_action( 'foundationpress_page_after_comments' ); ?>
 		</article>
-
-<?php		echo '<!-- c '.date(DATE_RFC2822).' -->' ;
-		$local_box_cache = ob_get_clean( );
-	// end the code to cache
-		echo $local_box_cache;
-	//end cache query 
-	
-	if( current_user_can( 'edit_post' ) ) {
-		//you cannot cache it
-	} else {
-		set_transient($box_q ,$local_box_cache, 60 * 10);
-	}
-} else { 
-
-echo $local_box_cache;
-
-}
-?>
-
-<?php } //end old template check ?>
-
-
-
 
 	<?php endwhile;?>
 
