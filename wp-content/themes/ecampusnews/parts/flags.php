73aeb2766726fb67c5fc1de92ba53d31bf0c2950
Erika Lewis <me.erikawithak@gmail.com>
2016-05-02 15:31:27 -0400

    upate flags

--- wp-content/themes/ecampusnews/parts/flags.php
+++ wp-content/themes/ecampusnews/parts/flags.php
@@ -2,21 +2,41 @@
 
 	$post_type = get_post_type( $post->ID ); 
 
-	if ($post_type == 'post') {
+	if (get_field('sponsored_article')) {
+		echo '<span class="flag content">';
+		echo '<a href="' . get_the_permalink() . '">';
+		echo 'Sponsored';
+		echo '</a></span>';
+
+	} elseif ($post_type == 'post') {
 
 		echo '<span class="flag content">';
 		echo '<a href="' . get_the_permalink() . '">';
 		echo 'News'; 
 		echo '</a></span>';
 
-	} elseif($post_type == 'entry') {
+	} elseif ($post_type == 'press-releases') {
 
 		echo '<span class="flag content">';
-		echo '<a href="' . get_site_url() .'/symposium'. '">';
-		echo 'Symposium'; 
-		echo '</a></span>'; 
+		echo '<a href="' . get_the_permalink() . '">';
+		echo 'Press Release'; 
+		echo '</a></span>';
+
+	} elseif ($post_type == 'staff') {
+
+		echo '<span class="flag content">';
+		echo '<a href="' . get_site_url() .'/staff'. '">';
+		echo 'Staff'; 
+		echo '</a></span>';
+
+	} elseif (is_page('current-issue')) {
+
+		echo '<span class="flag content">';
+		echo '<a href="' . get_site_url() .'/digital-issues'. '">';
+		echo 'Current Issue'; 
+		echo '</a></span>';
 	
-	} else {
+ 	} else {
 
 		$post_type = get_post_type_object( get_post_type($post) );
 
