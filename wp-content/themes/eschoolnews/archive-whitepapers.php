4fafaabc79473736a1a07412b2e90eb7fa79d2bd
Erika Lewis <me.erikawithak@gmail.com>
2016-05-03 16:19:36 -0400

    testing deploybot

--- wp-content/themes/eschoolnews/archive-whitepapers.php
+++ wp-content/themes/eschoolnews/archive-whitepapers.php
@@ -31,67 +31,67 @@ get_header();
 <script type="text/javascript">
 jQuery.noConflict()(function ($) { // this was missing for me
 $(document).ready(function(){
-		$("h4#EngagementTitle").hide();
-		$("h4#DataTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
 		$("h4#MobileTitle").hide();
 		$("h4#TechnologiesTitle").hide();	
 	$("#all").click(function(){
         $("div.row.all").show();
 		$("h4#AllTitle").show();
-		$("h4#EngagementTitle").hide();
-		$("h4#DataTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
 		$("h4#MobileTitle").hide();
 		$("h4#TechnologiesTitle").hide();		
 		
     });
 
-	$("#engagement").click(function(){
-		$("div.row.data-analytics-whitepapers").hide();
+	$("#curriculum").click(function(){
+		$("div.row.digital-whitepapers").hide();
 		$("div.row.technologies-whitepapers").hide();
-		$("div.row.online-mobile-whitepapers").hide();
-        $("div.row.engagement-retention-whitepapers").show();
+		$("div.row.mobile-online-whitepapers").hide();
+        $("div.row.curriculum-whitepapers").show();
 		$("h4#AllTitle").hide();
-		$("h4#EngagementTitle").show();
-		$("h4#DataTitle").hide();
+		$("h4#CurriculumTitle").show();
+		$("h4#DigitalTitle").hide();
 		$("h4#MobileTitle").hide();
 		$("h4#TechnologiesTitle").hide();		
 
     });
 
-	$("#data").click(function(){
-        $("div.row.engagement-retention-whitepapers").hide();
+	$("#digital").click(function(){
+        $("div.row.curriculum-whitepapers").hide();
 		$("div.row.technologies-whitepapers").hide();
-		$("div.row.online-mobile-whitepapers").hide();
-		$("div.row.data-analytics-whitepapers").show();
+		$("div.row.mobile-online-whitepapers").hide();
+		$("div.row.digital-whitepapers").show();
 		$("h4#AllTitle").hide();
-		$("h4#EngagementTitle").hide();
-		$("h4#DataTitle").show();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").show();
 		$("h4#MobileTitle").hide();
 		$("h4#TechnologiesTitle").hide();		
 
     });
 
 	$("#mobile").click(function(){
-        $("div.row.engagement-retention-whitepapers").hide();
-		$("div.row.data-analytics-whitepapers").hide();
+        $("div.row.curriculum-whitepapers").hide();
+		$("div.row.digital-whitepapers").hide();
 		$("div.row.technologies-whitepapers").hide();
-		$("div.row.online-mobile-whitepapers").show();
+		$("div.row.mobile-online-whitepapers").show();
 		$("h4#AllTitle").hide();
-		$("h4#EngagementTitle").hide();
-		$("h4#DataTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
 		$("h4#MobileTitle").show();
 		$("h4#TechnologiesTitle").hide();		
 
     });	
     
 	$("#technologies").click(function(){
-        $("div.row.engagement-retention-whitepapers").hide();
-		$("div.row.data-analytics-whitepapers").hide();
-		$("div.row.online-mobile-whitepapers").hide();
+        $("div.row.curriculum-whitepapers").hide();
+		$("div.row.digital-whitepapers").hide();
+		$("div.row.mobile-online-whitepapers").hide();
 		$("div.row.technologies-whitepapers").show();
 		$("h4#AllTitle").hide();
-		$("h4#EngagementTitle").hide();
-		$("h4#DataTitle").hide();
+		$("h4#CurriculumTitle").hide();
+		$("h4#DigitalTitle").hide();
 		$("h4#MobileTitle").hide();
 		$("h4#TechnologiesTitle").show();		
 
@@ -104,9 +104,9 @@ $(document).ready(function(){
 		
 		<ul class="stack-for-small button-group radius">
 			<li><a href="#menu" class="button" id="all">All White Papers</a></li>
-			<li><a href="#menu" class="button" id="data">Data &amp; Analytics</a></li>
-			<li><a href="#menu" class="button" id="engagement">Engagement &amp; Retention</a></li>
-			<li><a href="#menu" class="button" id="mobile">Online &amp; Mobile Learning</a></li>
+			<li><a href="#menu" class="button" id="curriculum">Curriculum</a></li>
+			<li><a href="#menu" class="button" id="digital">Digital</a></li>
+			<li><a href="#menu" class="button" id="mobile">Mobile &amp; Online Learning</a></li>
 			<li><a href="#menu" class="button" id="technologies">Technologies</a></li>
 		</ul>
 
@@ -126,9 +126,9 @@ $(document).ready(function(){
 <section <?PHP // role="tabpanel" aria-hidden="false" class="content active" id="panel1" ?>>
 		 
 		    <h4 id="AllTitle">All White Papers</h4>
-		    <h4 id="DataTitle" style="display:none;">Data &amp; Analytics</h4>
-		    <h4 id="EngagementTitle" style="display:none;">Engagement &amp; Retention</h4>
-            <h4 id="MobileTitle" style="display:none;">Online &amp; Mobile Learning</h4>
+		    <h4 id="CurriculumTitle" style="display:none;">Curriculum</h4>
+		    <h4 id="DigitalTitle" style="display:none;">Digital</h4>
+		    <h4 id="MobileTitle" style="display:none;">Mobile &amp; Online Learning</h4>
             <h4 id="TechnologiesTitle" style="display:none;">Technologies</h4>
 
 		    <br/>
@@ -148,7 +148,9 @@ $(document).ready(function(){
 				 while ( $query->have_posts() ) :
 					$query->the_post(); ?>
 
-					<div style="margin-bottom:8px;" class="panel row all<?php 
+				<div class="panel">
+
+					<header style="margin-bottom:8px;" class="row collapse all<?php 
 					$terms = wp_get_post_terms( $post->ID, 'subject_categories' );
 					foreach ( $terms as $term ) { echo " ".$term->slug ; } ?>">
 
@@ -158,20 +160,36 @@ $(document).ready(function(){
 
 							if (has_post_thumbnail()) { ?>
 
-							<div class="medium-4 columns">
+							<div class="medium-3 columns">
 
 							<?php the_post_thumbnail('medium-portrait'); ?>
 							
 							</div>
-                    	<div class="medium-8 columns">
+                    		
+                    		<div class="medium-9 columns">
+                    			<div class="row">
+                    				<div class="small-12 columns">
+                    					
+                    				
 
 						    <?php }else{ ?>
 
 						    <div class="medium-12 columns">
+						    	<div class="row collapse">
+                    				<div class="small-12 columns">
 
 						    <?php } ?>
                     	
-						<header>
+						
+							<?php 
+
+							// $taxonomy = 'flag';
+							// $terms = get_the_terms( $post->ID, $taxonomy);
+							// $term_name = $terms[0]->name;
+
+							// echo $term_name;
+
+							 ?>
                     		<h3><?php the_title(); ?></h3>
                     		<div class="posted-on"><?php the_time('F j, Y'); ?></div>
                     		<hr/>
@@ -218,29 +236,73 @@ $(document).ready(function(){
 											}
 										} ?>
 
+
+
+									
+										</div>
+                    				</div>
+
+
+
+
+	
+
 											</div>
 										</div>
 									<br/>
 									
 
 								<?php //endif; ?>
+
+
+
                     		
                     	</header>
 
+                    	<div class="row">
+                    		<div class="small-12 columns">
+
                     	<p class="excerpt">
 							<?php 
-							echo balanceTags(wp_trim_words( strip_tags(get_the_excerpt()), $num_words = 30, $more = '&hellip;' ), true); 
+							//echo balanceTags(wp_trim_words( strip_tags(get_the_excerpt()), $num_words = 30, $more = '&hellip;' ), true); 
+							the_content();
 							?>
 						</p>
+
+						<?php 
+
+						$posts = get_field('pdf_select');
+
+						if( $posts ): ?>
+						    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
+						        <?php setup_postdata($post); ?>
 						
 						<?php
 						$WPForm=get_post_meta($post->ID, 'WP Form Number', $single = true);
 
 						if ( esm_is_user_logged_in() and !$WPForm > 0) { ?>
-						
-						<div class="text-center">
-							<a class="button medium radius" href="<?php echo site_url(); ?>/<?php echo 'wp.php?wp='. get_the_ID();echo $aststr; ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'advanced' ), the_title_attribute( 'echo=0' ) ); ?>" target="_blank" id="submit">Download</a>
-						</div>
+
+							<?php 
+
+							$post_date = strtotime( the_date( 'Y-m-d', '', '', false ) );
+							$cutoff_date = strtotime( '2016-04-27' ); 
+
+							if( $post_date < $cutoff_date ) {
+
+							?>
+					
+						        <div class="text-center">
+									<a class="button radius" target="_blank" href="<?php the_permalink(); ?>">Read More</a>
+								</div>
+
+							<? } else { ?>
+
+								<div class="text-center">
+									<a class="button medium radius" href="<?php echo site_url(); ?>/<?php echo 'wp.php?wp='. get_the_ID();echo $aststr; ?>" rel="bookmark" title="<?php printf( esc_attr__( '%s', 'advanced' ), the_title_attribute( 'echo=0' ) ); ?>" target="_blank" id="submit">Download</a>
+								</div>
+
+							<?php } ?>
+						    
 
 						<?php } else { // not logged in ?>
 						
@@ -251,15 +313,24 @@ $(document).ready(function(){
                         
                         
                         <?php } ?>
+
+                        <?php endforeach; ?>
+						   
+						    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+						<?php endif; ?>
 					</div>
                     </div>
 
-                   
+
+
+                   </div>
 
 					
 					
 					<?php endwhile; ?>
 				<?php wp_reset_postdata(); ?>
+
+				
 				
 		  </section>
 		</div>
@@ -300,7 +371,6 @@ if (false === ($local_box_cache) ){
 echo $local_box_cache;
 
 }
-
 ?>
 
 
