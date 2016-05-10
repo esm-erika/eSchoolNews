5e1b90b09ca1540f9cd7be4d23a611ebaee60a28
vincentcarlson <vcarlson@eschoolmedia.com>
2016-05-10 12:40:44 -0400

    page google custom search test

---
+++ wp-content/themes/eschoolnews/page-googlecse.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Google Search
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+
+
+
+<div class="row">
+	<div class="small-12 large-12 columns" role="main">
+
+	<script>
+  (function() {
+    var cx = '007256987256189418192:esavkywrdf8';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:searchresults-only></gcse:searchresults-only>	
+	
+	</div>
+	
+<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
