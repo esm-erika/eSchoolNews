4a93662b2340eadfed0d5807f6cb589d1da8194f
vincentcarlson <vcarlson@eschoolmedia.com>
2016-05-10 13:51:25 -0400

    related content with tag name lookup for resource items

--- wp-content/themes/eschoolnews/parts/sidebar/related-content.php
+++ wp-content/themes/eschoolnews/parts/sidebar/related-content.php
@@ -9,7 +9,10 @@
     global $post;
     $tags = wp_get_post_tags($post->ID);
     $tag_ids = array();
+    $tag_slugs = array();
 	foreach($tags as $individual_tag){ $tag_ids[] = $individual_tag->term_id;}
+	foreach($slug as $individual_tagslug){ $tag_slugs[] = $individual_tag->slug;}
+	 
 
 	$cats = wp_get_post_categories($post->ID);
     $cat_ids = array();
@@ -22,7 +25,7 @@
 	
 			$args=array(
 			'post__not_in' => array($post->ID),
-			'posts_per_page'=>5, // Number of related posts to display.
+			'posts_per_page'=>3, // Number of related posts to display.
 			'ignore_sticky_posts'=>1,
 			'post_type' => array('post' ),  // 'whitepapers' ,'ercs' ,'webinars' ,'special-reports',
 			'category__in' => $cat_ids,
@@ -32,7 +35,7 @@
 		 } else if(!empty($tag_ids)){ 
 			$args=array(
 			'post__not_in' => array($post->ID),
-			'posts_per_page'=>5, // Number of related posts to display.
+			'posts_per_page'=>3, // Number of related posts to display.
 			'ignore_sticky_posts'=>1,
 			'post_type' => array('post' ),  // 'whitepapers' ,'ercs' ,'webinars' ,'special-reports',
 			'tag__in ' => $tag_ids,
@@ -89,6 +92,33 @@
 
 	 
 ?>	
+<?php 
+
+if(!empty($tag_slugs)){
+				 
+			$tagargs=array(
+			'post__not_in' => array($post->ID),
+			'posts_per_page'=>3, // Number of related posts to display.
+			'ignore_sticky_posts'=>1,
+			'post_type' => array('whitepapers' ,'ercs' ,'webinars' ,'special-reports' ),  // ,'post'
+			'tag_slug__and' => $tag_slugs,
+			);
+			
+			$mytag_query = new wp_query( $tagargs );
+
+}
+
+    while( $mytag_query->have_posts() ) {
+      $mytag_query->the_post();
+    ?>
+<li>
+<?php get_template_part('parts/flags'); ?>
+<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+</li>
+<?php 
+	 }
+?>
+
 
 </ul>
 </section>
