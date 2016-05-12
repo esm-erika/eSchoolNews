a57b6362ed2cb78f2abfdda391f0ddaff9151c10
Erika Lewis <me.erikawithak@gmail.com>
2016-05-12 11:28:15 -0400

    update twitter sidebar styles and layout

--- wp-content/themes/eschoolnews/library/widget-areas.php
+++ wp-content/themes/eschoolnews/library/widget-areas.php
@@ -13,10 +13,10 @@ function foundationpress_sidebar_widgets() {
 	  'id' => 'sidebar-widgets',
 	  'name' => __( 'Sidebar widgets', 'foundationpress' ),
 	  'description' => __( 'Drag widgets to this sidebar container.', 'foundationpress' ),
-	  'before_widget' => '<article id="%1$s" class="row widget %2$s"><div class="small-12 columns">',
-	  'after_widget' => '</div></article>',
-	  'before_title' => '<h3 class="section-title"><span>',
-	  'after_title' => '</span></h3>',
+	  'before_widget' => '<article id="%1$s" class="%2$s">',
+	  'after_widget' => '</article>',
+	  'before_title' => '<h5>',
+	  'after_title' => '</h5>',
 	));
 
 	register_sidebar(array(
