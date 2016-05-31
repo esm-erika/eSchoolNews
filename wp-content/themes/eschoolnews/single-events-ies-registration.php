96e9386b1167a2998b7ce32399a904db8b32f7b0
Erika Lewis <me.erikawithak@gmail.com>
2016-05-31 17:16:37 -0400

    adding template file and conditional time

---
+++ wp-content/themes/eschoolnews/single-events-ies-registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* The template for displaying all single posts and attachments
+*
+* @package WordPress
+* @subpackage FoundationPress
+* @since FoundationPress 1.0.0
+*/
+
+get_header(); ?>
+
+<div class="row">
+	<div class="small-12 columns">
+		
+	This is a page template.
+
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
