c0abae2a156bb9e11421a2823f189da5f0bba3f2
Erika Lewis <me.erikawithak@gmail.com>
2016-05-31 17:59:17 -0400

    debugging conferences sidebar

--- wp-content/themes/eschoolnews/parts/sidebar/conferences.php
+++ wp-content/themes/eschoolnews/parts/sidebar/conferences.php
@@ -4,27 +4,9 @@
 
 <ul class="small-block-grid-1 medium-block-grid-2">
 
-	<?php
 
-	$taxonomy = 'conferences';
-	$terms = get_terms( $taxonomy );
 
-	if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-
-    echo '<ul>';
-    
-    foreach ( $terms as $term ) {
-
-    	$term_link = get_term_link( $term->term_id, $taxonomy);
-
-        echo '<li><a href="' . esc_url($term_link) . '">' . $term->name . '</a></li>';
-    }
-    
-    echo '</ul>';
-	
-	} ?>
-
-	</ul>
+</ul>
 
 
 </article>
