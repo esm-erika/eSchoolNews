72a219f5729c421b52d13f2a00ac98a3b0b20a84
erikawithak <me.erikawithak@gmail.com>
2016-06-07 15:08:37 -0400

    change related articles to a panel

--- wp-content/themes/eschoolnews/single-events.php
+++ wp-content/themes/eschoolnews/single-events.php
@@ -163,7 +163,7 @@ if (false === ($local_box_cache) ){
 
 					<?php if ( $article_query->have_posts() ) : ?>
 
-					<hr>
+					<div class="panel">
 
 					<h4><?php the_title(); ?> Related Articles</h4>
 					<ul class="large-block-grid-3">
@@ -221,6 +221,8 @@ if (false === ($local_box_cache) ){
 				</ul>
 
 			<?php endif; ?>
+
+		</div>
 
 					
 					
