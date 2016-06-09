29fb975671332713c89d48048288754a64d182b0
erikawithak <me.erikawithak@gmail.com>
2016-06-09 13:24:22 -0400

    adding links at top

--- wp-content/themes/eschoolnews/parts/top-bar.php
+++ wp-content/themes/eschoolnews/parts/top-bar.php
@@ -11,14 +11,55 @@
 <br>
 <div class="top-bar-container contain-to-grid show-for-medium-up">
 
-        <div class="title-area row show-for-large-up">
-            <div class="name large-6 large-centered columns">
-                <div class="row show-for-large-up" style="height: 20px;"></div>
-                <h1 class="large-10 large-centered columns">
-                    <a href="<?php echo home_url(); ?>">
-                        
-<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>" />                    </a>
-                </h1>
+        <div class="title-area row show-for-large-up" data-equalizer>
+            <div class="large-3 columns text-center" data-equalizer-watch>
+
+                <div style="height: 100%; position: relative;">
+                    <div style="positon: absolute; width: 100%; height: 100%; display: table;">
+                        <div style="display: table-cell; vertical-align: middle;">
+
+                <?php if ( is_user_logged_in() ) { 
+                    
+                        $current_user = wp_get_current_user();
+                        echo '<a href="' . site_url() . '/eschoolnews-today/">';
+                        echo 'Today\'s News Brief for <br> <strong>';
+                        echo $current_user->user_firstname;
+                        echo ' ';
+                        echo $current_user->user_lastname;
+                        echo '</strong></a><br>';
+
+                ?>
+                    
+                <?php } else { 
+
+                    echo '<a href="' . site_url() . '/eschoolnews-today/">See Today\'s News Brief</a>';
+
+                 } ?>
+
+                </div>
+                </div>
+                </div>
+
+            </div>
+            <div class="name large-6 columns" data-equalizer-watch>
+                <div class="show-for-large-up" style="height: 20px;"></div>
+                    <h1 class="text-center">
+                        <a href="<?php echo home_url(); ?>">
+                            <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php bloginfo( 'name' ); ?>" style="max-width: 400px;" />                    
+                        </a>
+                    </h1>
+                </div>
+            <div class="large-3 columns text-center" data-equalizer-watch>
+
+                <div style="height: 100%; position: relative;">
+                    <div style="positon: absolute; width: 100%; height: 100%; display: table;">
+                        <div style="display: table-cell; vertical-align: middle;">
+
+                            Subscribe box
+
+                    </div>
+                    </div>
+                    </div>
             </div>
         </div>
 
